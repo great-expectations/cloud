b7e314387626a205b39c0f02dbe2236d4a38c8c6
Gabriel Gore <gabriel59kg@gmail.com>
2023-07-14 19:30:24 -0400

    setup basic dev tasks

---
+++ tasks.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import invoke
+
+if TYPE_CHECKING:
+    from invoke import Context
+
+
+@invoke.task
+def fmt(ctx: Context, check: bool = False):
+    """Format code with black"""
+    cmds = ["black", "."]
+    if check:
+        cmds.append("--check")
+    ctx.run(" ".join(cmds), echo=True, pty=True)
+
+
+@invoke.task
+def lint(ctx: Context, check: bool = False):
+    """Lint and fix code with ruff"""
+    cmds = ["ruff", "."]
+    if not check:
+        cmds.append("--fix")
+    ctx.run(" ".join(cmds), echo=True, pty=True)
+
+
+@invoke.task(
+    aliases=["types"],
+)
+def type_check(ctx: Context, install_types: bool = False, check: bool = False):
+    """Type check code with mypy"""
+    cmds = ["mypy"]
+    if install_types or check:
+        cmds.append("--install-types")
+    ctx.run(" ".join(cmds), echo=True, pty=True)
