0cee6664e99dff138fbfd3cb7b2cc733b4787f64
Gabriel <gabriel59kg@gmail.com>
2023-07-14 20:30:11 -0400

    Project scaffold (#1)
    Project scaffolding with poetry, mypy, ruff, github actions and pre-commit ci.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cloud
+[![ci](https://github.com/great-expectations/cloud/actions/workflows/ci.yaml/badge.svg)](https://github.com/great-expectations/cloud/actions/workflows/ci.yaml?query=branch%3Adevelop)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/great-expectations/cloud/main.svg)](https://results.pre-commit.ci/latest/github/great-expectations/cloud/main)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/charliermarsh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+## Dev Setup
+
+1. [Install `poetry`](https://python-poetry.org/docs/#installation)
+    * [`pipx install poetry`](https://python-poetry.org/docs/#installing-with-pipx)
+2. Setup virtual environment and install dependencies.
+    * `poetry install --sync`
+3. Activate your virtual environment.
+    * `poetry shell`
+
+### Developer Tasks
+
+Common developer tasks are available via `invoke` (defined in `tasks.py`)
+
+`invoke --list` to see available tasks.
---
+++ tasks.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import invoke
+
+if TYPE_CHECKING:
+    from invoke import Context
+
+
+@invoke.task
+def fmt(ctx: Context, check: bool = False):
+    """Format code with black"""
+    cmds = ["black", "."]
+    if check:
+        cmds.append("--check")
+    ctx.run(" ".join(cmds), echo=True, pty=True)
+
+
+@invoke.task
+def lint(ctx: Context, check: bool = False):
+    """Lint and fix code with ruff"""
+    cmds = ["ruff", "."]
+    if not check:
+        cmds.append("--fix")
+    ctx.run(" ".join(cmds), echo=True, pty=True)
+
+
+@invoke.task(
+    aliases=["types"],
+)
+def type_check(ctx: Context, install_types: bool = False, check: bool = False):
+    """Type check code with mypy"""
+    cmds = ["mypy"]
+    if install_types:
+        cmds.append("--install-types")
+    if check:
+        cmds.append("--pretty")
+    ctx.run(" ".join(cmds), echo=True, pty=True)
+
+
+@invoke.task
+def deps(ctx: Context):
+    """Sync dependencies with poetry lock file"""
+    cmds = ["poetry", "install", "--sync"]
+    ctx.run(" ".join(cmds), echo=True, pty=True)
