72cc6e2ba632a30a762ff35a575d9b320c8e1d3c
Chetan Kini <chetan@superconductive.com>
2023-09-21 09:56:29 -0400

    fix import

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -5,7 +5,8 @@ from unittest.mock import call
 
 import pytest
 
-from great_expectations_cloud.agent import GXAgent, GXAgentConfig
+from great_expectations_cloud.agent import GXAgent
+from great_expectations_cloud.agent.agent import GXAgent
 from great_expectations_cloud.agent.actions.agent_action import ActionResult
 from great_expectations_cloud.agent.message_service.asyncio_rabbit_mq_client import (
     ClientError,
