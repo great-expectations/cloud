d1816db169c86d47ed8fe20578f52fac7e8656d2
Gabriel Gore <gabriel59kg@gmail.com>
2023-10-05 09:56:03 -0400

    allow config from a json file

--- great_expectations_cloud/logging_cfg.py
+++ great_expectations_cloud/logging_cfg.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 import enum
+import json
 import logging
+import logging.config
+import pathlib
 
 from typing_extensions import override
 
@@ -32,6 +35,20 @@ class LogLevel(str, enum.Enum):
         return logging.getLevelName(self)
 
 
-def configure_logger(log_level: LogLevel, **logger_config_kwargs) -> None:
-    logging.basicConfig(level=log_level.numeric_level, **logger_config_kwargs)
-    LOGGER.info(f"Configured logging at level {log_level}")
+def configure_logger(
+    log_level: LogLevel, log_cfg_file: pathlib.Path | None, **logger_config_kwargs
+) -> None:
+    """
+    Configure the root logger for the application.
+    If a log configuration file is provided, other arguments are ignored.
+
+    See the documentation for the logging.config.dictConfig method for details.
+    https://docs.python.org/3/library/logging.config.html#logging-config-dictschema
+    """
+    if log_cfg_file:
+        dict_config = json.loads(log_cfg_file.read_text())
+        logging.config.dictConfig(dict_config)
+        LOGGER.info(f"Configured logging from file {log_cfg_file}")
+    else:
+        logging.basicConfig(level=log_level.numeric_level, **logger_config_kwargs)
+        LOGGER.info(f"Configured logging at level {log_level}")
