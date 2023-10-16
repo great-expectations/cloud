7bda589664fc914c2a2be2f2e50c779539e7354e
Gabriel <gabriel59kg@gmail.com>
2023-10-16 14:14:53 -0400

    mypy 1.6 (#38)

--- great_expectations_cloud/agent/config.py
+++ great_expectations_cloud/agent/config.py
@@ -1,9 +1,9 @@
-from great_expectations.compatibility.pydantic import AnyUrl, BaseSettings
-from great_expectations.data_context.cloud_constants import CLOUD_DEFAULT_BASE_URL
+from great_expectations.compatibility.pydantic import BaseSettings
 
 
 class GxAgentEnvVars(BaseSettings):  # type: ignore[misc] # BaseSettings is has Any type
     # pydantic will coerce this string to AnyUrl type
-    gx_cloud_base_url: AnyUrl = CLOUD_DEFAULT_BASE_URL
+    # gx_cloud_base_url: AnyUrl = CLOUD_DEFAULT_BASE_URL
+    gx_cloud_base_url: str
     gx_cloud_organization_id: str
     gx_cloud_access_token: str
