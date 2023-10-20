49636b9e07f2f9779f846be4b44e18e63a15e7c3
Gabriel Gore <gabriel59kg@gmail.com>
2023-10-20 12:41:14 -0400

    version task (v1)

--- tasks.py
+++ tasks.py
@@ -1,13 +1,42 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import functools
+import logging
+import pathlib
+from pprint import pformat as pf
+from typing import TYPE_CHECKING, Final, Literal
 
 import invoke
+import requests
+import tomli
+import tomli_w
+from packaging.version import Version
 
 if TYPE_CHECKING:
-    from invoke import Context
+    from invoke.context import Context
 
-DOCKERFILE_PATH = "great_expectations_cloud/agent/Dockerfile"
+LOGGER: Final[logging.Logger] = logging.getLogger(__name__)
+
+PROJECT_ROOT: Final[pathlib.Path] = pathlib.Path(__file__).parent
+PYPROJECT_TOML: Final[pathlib.Path] = PROJECT_ROOT / "pyproject.toml"
+DOCKERFILE_PATH: Final[pathlib.Path] = PROJECT_ROOT / "agent" / " Dockerfile"
+
+
+@functools.lru_cache(maxsize=8)
+def _get_pyproject_tool_dict(
+    tool_key: Literal["poetry", "black", "ruff", "mypy"] | None = None
+) -> dict:
+    """
+    Load the pyproject.toml file as a dict. Optional return the config of a specific tool.
+    Caches each tool's config for faster access.
+    """
+    assert PYPROJECT_TOML.exists()
+    pyproject_dict = tomli.loads(PYPROJECT_TOML.read_text())
+    LOGGER.warning(f"pyproject.toml ->\n {pf(pyproject_dict, depth=2)}")
+    tool_dict = pyproject_dict["tool"]
+    if tool_key:
+        return tool_dict[tool_key]
+    return tool_dict
 
 
 @invoke.task
@@ -53,10 +82,10 @@ def docker(ctx: Context, check: bool = False, tag: str = "greatexpectations/agen
             "DL3029",  # Revisit support for arm platform builds https://github.com/hadolint/hadolint/wiki/DL3029
             "-",
             "<",
-            DOCKERFILE_PATH,
+            str(DOCKERFILE_PATH),
         ]
     else:
-        cmds = ["docker", "build", "-f", DOCKERFILE_PATH, "-t", tag, "."]
+        cmds = ["docker", "build", "-f", str(DOCKERFILE_PATH), "-t", tag, "."]
     ctx.run(" ".join(cmds), echo=True, pty=True)
 
 
@@ -94,3 +123,56 @@ def build(ctx: Context) -> None:
         ".",
     ]
     ctx.run(" ".join(cmds), echo=True, pty=True)
+
+
+def _get_local_version() -> Version:
+    return Version(_get_pyproject_tool_dict("poetry")["version"])
+
+
+def _get_latest_version() -> Version:
+    r = requests.get("https://pypi.org/pypi/great-expectations-cloud/json")
+    r.raise_for_status()
+    version = Version(r.json()["info"]["version"])
+    return version
+
+
+def _bump_version(version_: Version, full_release: bool = False) -> Version:
+    if full_release:
+        new_version = Version(f"{version_.major}.{version_.minor}.{version_.micro + 1}")
+    elif version_.dev:
+        new_version = Version(
+            f"{version_.major}.{version_.minor}.{version_.micro}dev{version_.dev + 1}"
+        )
+    else:
+        raise NotImplementedError
+    return new_version
+
+
+def _update_version(version_: Version | str) -> None:
+    """
+    Modify the pyproject.toml version.
+    """
+    if not isinstance(version_, Version):
+        version_ = Version(version_)
+    # TODO: open file once
+    # TODO: use tomlkit to preserve comments and formatting
+    with open(PYPROJECT_TOML, "rb") as f_in:
+        full_toml_dict = tomli.load(f_in)
+    with open(PYPROJECT_TOML, "wb") as f_out:
+        full_toml_dict["tool"]["poetry"]["version"] = str(version_)
+        tomli_w.dump(full_toml_dict, f_out)
+
+
+@invoke.task
+def version(ctx: Context, bump: bool = False, full_release: bool = False) -> None:
+    """Bump the version of the project."""
+    local_version = _get_local_version()
+    print(f"local: \t\t{local_version}")
+    latest_version = _get_latest_version()
+    print(f"pypi latest: \t{latest_version}")
+
+    if bump:
+        print("\n  bumping version", end=" ")
+        new_version = _bump_version(local_version, full_release=full_release)
+        _update_version(new_version)
+        print(f"\nnew version: \t{new_version}")
