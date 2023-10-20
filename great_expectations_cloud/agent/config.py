f7b61594d9ac7b62ee2d0ba6dd1e84bd26026e45
Allen Sallinger <allen@superconductive.com>
2023-10-20 09:44:43 -0700

    add back AnyUrl and figure out test pathing

--- great_expectations_cloud/agent/config.py
+++ great_expectations_cloud/agent/config.py
@@ -1,9 +1,8 @@
-from great_expectations.compatibility.pydantic import BaseSettings
+from great_expectations.compatibility.pydantic import AnyUrl, BaseSettings
 
 
 class GxAgentEnvVars(BaseSettings):  # type: ignore[misc] # BaseSettings is has Any type
     # pydantic will coerce this string to AnyUrl type
-    # gx_cloud_base_url: AnyUrl = CLOUD_DEFAULT_BASE_URL
-    gx_cloud_base_url: str
+    gx_cloud_base_url: AnyUrl
     gx_cloud_organization_id: str
     gx_cloud_access_token: str
