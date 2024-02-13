a03d6d8636dea6604bb54cecd50a3457946dd6af
Gabriel Gore <gabriel59kg@gmail.com>
2024-02-12 22:36:51 -0500

    define `OnMessageCallback` Protocol

--- great_expectations_cloud/agent/message_service/asyncio_rabbit_mq_client.py
+++ great_expectations_cloud/agent/message_service/asyncio_rabbit_mq_client.py
@@ -5,7 +5,7 @@ import ssl
 from asyncio import AbstractEventLoop
 from dataclasses import dataclass
 from functools import partial
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, Protocol
 
 import pika
 from pika.adapters.asyncio_connection import AsyncioConnection
@@ -22,6 +22,11 @@ class OnMessagePayload:
     body: bytes
 
 
+class OnMessageCallback(Protocol):
+    def __call__(self, payload: OnMessagePayload) -> None:
+        ...
+
+
 class AsyncRabbitMQClient:
     """Configuration for a particular AMQP client library."""
 
@@ -35,7 +40,7 @@ class AsyncRabbitMQClient:
         self._consumer_tag = None
         self._consuming = False
 
-    def run(self, queue: str, on_message: Callable[[OnMessagePayload], None]) -> None:
+    def run(self, queue: str, on_message: OnMessageCallback) -> None:
         """Run an async connection to RabbitMQ.
 
         Args:
@@ -145,7 +150,7 @@ class AsyncRabbitMQClient:
         method_frame: Basic.Deliver,
         header_frame: BasicProperties,
         body: bytes,
-        on_message: Callable[[OnMessagePayload], None],
+        on_message: OnMessageCallback,
     ) -> None:
         """Called by Pika when a message is received."""
         # param on_message is provided by the caller as an argument to AsyncRabbitMQClient.run
@@ -156,7 +161,7 @@ class AsyncRabbitMQClient:
         )
         return on_message(payload)
 
-    def _start_consuming(self, queue: str, on_message: Callable, channel: Channel) -> None:
+    def _start_consuming(self, queue: str, on_message: OnMessageCallback, channel: Channel) -> None:
         """Consume from a channel with the on_message callback."""
         channel.add_on_cancel_callback(self._on_consumer_canceled)
         self._consumer_tag = channel.basic_consume(queue=queue, on_message_callback=on_message)
@@ -183,7 +188,7 @@ class AsyncRabbitMQClient:
             self._channel.close()
 
     def _on_connection_open(
-        self, connection: AsyncioConnection, queue: str, on_message: Callable
+        self, connection: AsyncioConnection, queue: str, on_message: OnMessageCallback
     ) -> None:
         """Callback invoked after the broker opens the connection."""
         on_channel_open = partial(self._on_channel_open, queue=queue, on_message=on_message)
@@ -209,7 +214,7 @@ class AsyncRabbitMQClient:
         else:
             self._connection.close()
 
-    def _on_channel_open(self, channel: Channel, queue: str, on_message: Callable) -> None:
+    def _on_channel_open(self, channel: Channel, queue: str, on_message: OnMessageCallback) -> None:
         """Callback invoked after the broker opens the channel."""
         self._channel = channel
         channel.add_on_close_callback(self._on_channel_closed)
