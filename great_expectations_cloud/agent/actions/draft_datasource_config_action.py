b3129cf1e633e3e2666695d3d57144123d6ed883
Anthony Burdi <anthony@greatexpectations.io>
2024-02-27 19:37:21 -0500

    Revert draft_datasource_config_action.py changes

--- great_expectations_cloud/agent/actions/draft_datasource_config_action.py
+++ great_expectations_cloud/agent/actions/draft_datasource_config_action.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable
+from typing import Any
 from uuid import UUID
 
-import great_expectations as gx
 from great_expectations.compatibility import pydantic
 from great_expectations.core.http import create_session
-from packaging.version import Version
 from typing_extensions import override
 
 from great_expectations_cloud.agent.actions import ActionResult, AgentAction
@@ -15,42 +13,6 @@ from great_expectations_cloud.agent.config import (
     generate_config_validation_error_text,
 )
 from great_expectations_cloud.agent.models import DraftDatasourceConfigEvent
-
-if TYPE_CHECKING:
-    from great_expectations.data_context import CloudDataContext
-
-if TYPE_CHECKING:
-    from great_expectations_cloud.agent.models import Event
-
-GX_VERSION = Version(gx.__version__)
-
-
-# TODO: Placeholder for datasource config action registration / implementation. Consider moving to a better place.
-#  Also consider moving to a class-based implementation and only doing the version check once on load.
-def lookup_runner(context: CloudDataContext) -> Callable:  # TODO: Return signature?
-    """Lookup the correct runner implementation based on the context version."""
-
-    # if Version("0.18.0") <= GX_VERSION < Version("1.0.0"):
-    #     return run_checkpoint_v0_18
-    # elif GX_VERSION >= Version("1.0.0"):
-    #     return run_checkpoint_v1_0
-    raise NotImplementedError
-
-
-def draft_datasource_config_action_impl(
-    context: CloudDataContext, event: Event, id: str
-) -> ActionResult:
-    # TODO: Should the lookup be cached?
-    # TODO: Should the lookup be of a class instead of a function? E.g. a class tied to the context version?
-    runner = lookup_runner(context)
-    return runner(context, event, id)
-
-
-def draft_datasource_config_action_v0_18(
-    context: CloudDataContext, event: Event, id: str
-) -> ActionResult:
-    # TODO: The code from the run() method in DraftDatasourceConfigAction should be moved here.
-    raise NotImplementedError
 
 
 class DraftDatasourceConfigAction(AgentAction[DraftDatasourceConfigEvent]):
