6906f5b228e9e3fd76e7fe65d5d9b819f10d3a5b
Robby Reinold <robby@greatexpectations.io>
2024-03-01 12:50:15 -0600

    add unknown action

---
+++ great_expectations_cloud/agent/actions/unknown.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing_extensions import override
+
+from great_expectations_cloud.agent.actions.agent_action import (
+    ActionResult,
+    AgentAction,
+)
+from great_expectations_cloud.agent.models import (
+    RunCheckpointEvent,
+    UnknownEvent,
+)
+
+
+class UnknownEventAction(AgentAction[UnknownEvent]):
+    @override
+    def run(self, event: RunCheckpointEvent, id: str) -> ActionResult:
+        # noop
+        return ActionResult(
+            id=id,
+            type=event.type,
+            created_resources=[],
+        )
