02ba246d9678ac44d86db630684c0c85f83dbf8e
Robby Reinold <robby@greatexpectations.io>
2024-03-04 11:03:38 -0500

    add 3 parse tests

--- tests/agent/message_service/test_subscriber.py
+++ tests/agent/message_service/test_subscriber.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import json
 from unittest.mock import ANY, Mock
 
 import pytest
@@ -11,9 +11,18 @@ from great_expectations_cloud.agent.message_service.subscriber import (
     EventContext,
     Subscriber,
 )
+from great_expectations_cloud.agent.models import RunOnboardingDataAssistantEvent
 from tests.agent.message_service.amqp_errors import get_amqp_errors
 
+@pytest.fixture()
+def mock_subscriber():
+    return Subscriber(client=Mock(autospec=AsyncRabbitMQClient))
 
+
+@pytest.fixture()
+def example_event():
+    return RunOnboardingDataAssistantEvent(type="onboarding_data_assistant_request.received", datasource_name="abc",
+                                            data_asset_name="boo")
 def test_subscriber_consume_calls_run():
     client = Mock(autospec=AsyncRabbitMQClient)
     client.should_reconnect = False  # avoid infinite loop
@@ -63,10 +72,28 @@ def test_subscriber_close_handles_amqp_errors_from_connection(error):
 
     subscriber.close()  # no exception
 
-def test_subscriber_parse_event():
-    subscriber = Subscriber(client=Mock(autospec=AsyncRabbitMQClient))
-
-    event = subscriber.parse_event_from(b'{"type": "test"}')
+def test_subscriber_parse_event_extra_field(mock_subscriber, example_event):
+    event_dict = dict(example_event)
+    event_dict["new_field"] = "surprise!"
+    serialized_bytes = json.dumps(dict(event_dict), indent=2).encode('utf-8')
+    event = mock_subscriber.parse_event_from(serialized_bytes)
 
     assert event.type == "unknown_event"
-    assert event.dict() == {"type": "test"}
+
+
+def test_subscriber_parse_event_extra_field(mock_subscriber, example_event):
+    event_dict = dict(example_event)
+    del event_dict["datasource_name"]
+    serialized_bytes = json.dumps(dict(event_dict), indent=2).encode('utf-8')
+    event = mock_subscriber.parse_event_from(serialized_bytes)
+
+    assert event.type == "unknown_event"
+
+def test_subscriber_parse_event(mock_subscriber, example_event):
+    event_dict = dict(example_event)
+    serialized_bytes = json.dumps(dict(event_dict), indent=2).encode('utf-8')
+    event = mock_subscriber.parse_event_from(serialized_bytes)
+
+    assert event.type == "onboarding_data_assistant_request.received"
+
+
