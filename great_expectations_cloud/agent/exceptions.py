07eede6e81d81ba4721ad56bcc9804a9e62c24a3
Anthony Burdi <anthony@greatexpectations.io>
2024-03-04 15:47:45 -0500

    Add raise_with_error_code

--- great_expectations_cloud/agent/exceptions.py
+++ great_expectations_cloud/agent/exceptions.py
@@ -1,6 +1,14 @@
 from __future__ import annotations
 
 
+def raise_with_error_code(e: Exception, error_code: str) -> None:
+    """Raise a GXCoreError with the given error_code and the message from the given exception.
+
+    Meant to standardize the way we raise GXCoreErrors with error codes.
+    """
+    raise GXCoreError(message=str(e), error_code=error_code) from e
+
+
 class GXCoreError(Exception):
     """To bridge between 0.18.x and 1.0 we create the GXCoreError base class in the agent code
     and add additional properties if relevant when subclassing. This should be moved to the
