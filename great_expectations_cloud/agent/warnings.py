2009bce8af8a5590a9f9c28a6c6762d5ce2b0209
Robby Reinold <robby@greatexpectations.io>
2024-03-04 20:33:00 -0500

    move warns

--- great_expectations_cloud/agent/warnings.py
+++ great_expectations_cloud/agent/warnings.py
@@ -2,17 +2,14 @@ from __future__ import annotations
 
 import warnings
 
-import pytest
-
 
 class GXAgentUserWarning(UserWarning):
     ...
 
 
 def warn_unknown_event(event_type: str) -> None:
-    with pytest.warns(GXAgentUserWarning):
-        warnings.warn(
-            message=f"The version of the GX Agent you are using does not support this job ({event_type}). Please upgrade to latest.",
-            category=GXAgentUserWarning,
-            stacklevel=2,
-        )
+    warnings.warn(
+        message=f"The version of the GX Agent you are using does not support this job ({event_type}). Please upgrade to latest.",
+        category=GXAgentUserWarning,
+        stacklevel=2,
+    )
--- tests/agent/actions/test_unknown.py
+++ tests/agent/actions/test_unknown.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 from unittest.mock import Mock
 
+import pytest
+
 from great_expectations_cloud.agent.actions.unknown import UnknownEventAction
 from great_expectations_cloud.agent.models import UnknownEvent
+from great_expectations_cloud.agent.warnings import GXAgentUserWarning
 
 
 def test_unknown_throws_warning():
     event = UnknownEvent()
     action = UnknownEventAction(context=Mock())
-    # with pytest.warns(GXAgentUserWarning):
-    action.run(event=event, id="lala")
+    with pytest.warns(GXAgentUserWarning):
+        action.run(event=event, id="lala")
