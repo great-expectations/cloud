c14e280b9afa7b160ba13b86cd8bfa6afd97b3e6
Gabriel Gore <gabriel59kg@gmail.com>
2024-03-13 20:36:40 -0400

    add back tests

---
+++ tests/agent/actions/test_draft_datasource_config.py
@@ -0,0 +1,224 @@
+from __future__ import annotations
+
+import uuid
+from typing import TYPE_CHECKING, Any, Literal
+from uuid import UUID
+
+import pytest
+from great_expectations.datasource.fluent import SQLDatasource
+from great_expectations.datasource.fluent.interfaces import TestConnectionError
+
+from great_expectations_cloud.agent.actions.draft_datasource_config_action import (
+    DraftDatasourceConfigAction,
+)
+from great_expectations_cloud.agent.config import GxAgentEnvVars
+from great_expectations_cloud.agent.exceptions import ErrorCode, GXCoreError
+from great_expectations_cloud.agent.models import DraftDatasourceConfigEvent
+
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
+
+
+@pytest.fixture
+def org_id():
+    return "81f4e105-e37d-4168-85a0-2526943f9956"
+
+
+@pytest.fixture
+def token():
+    return "MTg0NDkyYmYtNTBiOS00ZDc1LTk3MmMtYjQ0M2NhZDA2NjJk"
+
+
+@pytest.fixture
+def set_required_env_vars(monkeypatch, org_id, token) -> None:
+    env_vars = {
+        "GX_CLOUD_ORGANIZATION_ID": org_id,
+        "GX_CLOUD_ACCESS_TOKEN": token,
+    }
+    for key, val in env_vars.items():
+        monkeypatch.setenv(name=key, value=val)
+
+
+def build_payload(
+    config: dict[str, Any], id: UUID
+) -> dict[Literal["data"], dict[str, str | UUID | dict[str, Any]]]:
+    return {
+        "data": {
+            "type": "draft_config",
+            "id": id,
+            "attributes": {"draft_config": config},
+        }
+    }
+
+
+def test_test_draft_datasource_config_success(
+    mock_context, mocker: MockerFixture, set_required_env_vars: None
+):
+    datasource_config = {"type": "pandas", "name": "test-1-2-3"}
+    config_id = UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5")
+    create_session = mocker.patch(
+        "great_expectations_cloud.agent.actions.draft_datasource_config_action.create_session"
+    )
+    session = create_session.return_value
+    response = session.get.return_value
+    response.ok = True
+    response.json.return_value = build_payload(config=datasource_config, id=config_id)
+    env_vars = GxAgentEnvVars()
+    action = DraftDatasourceConfigAction(context=mock_context)
+    job_id = UUID("87657a8e-f65e-4e64-b21f-e83a54738b75")
+    event = DraftDatasourceConfigEvent(config_id=config_id)
+    expected_url: str = (
+        f"{env_vars.gx_cloud_base_url}/organizations/{env_vars.gx_cloud_organization_id}"
+        f"/datasources/drafts/{config_id}"
+    )
+
+    action_result = action.run(event=event, id=str(job_id))
+
+    assert action_result.id == str(job_id)
+    assert action_result.type == event.type
+    assert action_result.created_resources == []
+
+    session.get.assert_called_with(expected_url)
+
+
+def test_test_draft_datasource_config_failure(
+    mock_context, mocker: MockerFixture, set_required_env_vars: None
+):
+    ds_type = "sql"
+    datasource_config = {"type": ds_type, "name": "test-1-2-3"}
+    config_id = UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5")
+    create_session = mocker.patch(
+        "great_expectations_cloud.agent.actions.draft_datasource_config_action.create_session"
+    )
+    session = create_session.return_value
+    response = session.get.return_value
+    response.ok = True
+    response.json.return_value = build_payload(config=datasource_config, id=config_id)
+    env_vars = GxAgentEnvVars()
+    action = DraftDatasourceConfigAction(context=mock_context)
+    job_id = UUID("87657a8e-f65e-4e64-b21f-e83a54738b75")
+    event = DraftDatasourceConfigEvent(config_id=config_id)
+    expected_url = (
+        f"{env_vars.gx_cloud_base_url}/organizations/{env_vars.gx_cloud_organization_id}"
+        f"/datasources/drafts/{config_id}"
+    )
+    datasource_cls = mocker.Mock(autospec=SQLDatasource)
+    mock_context.sources.type_lookup = {ds_type: datasource_cls}
+    datasource_cls.return_value.test_connection.side_effect = TestConnectionError
+
+    with pytest.raises(GXCoreError):
+        action.run(event=event, id=str(job_id))
+
+    session.get.assert_called_with(expected_url)
+
+
+def test_test_draft_datasource_config_raises_for_non_fds(
+    mock_context, mocker: MockerFixture, set_required_env_vars: None
+):
+    datasource_config = {"name": "test-1-2-3", "connection_string": ""}
+    config_id = UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5")
+    create_session = mocker.patch(
+        "great_expectations_cloud.agent.actions.draft_datasource_config_action.create_session"
+    )
+    session = create_session.return_value
+    response = session.get.return_value
+    response.ok = True
+    response.json.return_value = build_payload(config=datasource_config, id=config_id)
+    env_vars = GxAgentEnvVars()
+    action = DraftDatasourceConfigAction(context=mock_context)
+    job_id = UUID("87657a8e-f65e-4e64-b21f-e83a54738b75")
+    event = DraftDatasourceConfigEvent(config_id=config_id)
+    expected_url = (
+        f"{env_vars.gx_cloud_base_url}/organizations/{env_vars.gx_cloud_organization_id}"
+        f"/datasources/drafts/{config_id}"
+    )
+    with pytest.raises(TypeError, match="fluent-style Data Source"):
+        action.run(event=event, id=str(job_id))
+
+    session.get.assert_called_with(expected_url)
+
+
+@pytest.mark.parametrize(
+    "error_message, expected_error_code",
+    [
+        (
+            """Attempt to connect to datasource failed with the following error message: (snowflake.connector.errors.DatabaseError) 250001 (08001): None: Failed to connect to DB: <DB Name> Incorrect username or password was specified.\n(Background on this error at: https://sqlalche.me/e/14/4xp6)""",
+            ErrorCode.WRONG_USERNAME_OR_PASSWORD,
+        ),
+        (
+            """Unrecognized error.""",
+            ErrorCode.GENERIC_UNHANDLED_ERROR,
+        ),
+    ],
+)
+def test_draft_datasource_config_failure_raises_correct_gx_core_error(
+    mock_context, mocker: MockerFixture, error_message: str, expected_error_code: str
+):
+    action = DraftDatasourceConfigAction(context=mock_context)
+    mock_check_draft_datasource_config = mocker.patch(
+        f"{DraftDatasourceConfigAction.__module__}.{DraftDatasourceConfigAction.__name__}.check_draft_datasource_config"
+    )
+    mock_check_draft_datasource_config.side_effect = TestConnectionError(error_message)
+
+    event = DraftDatasourceConfigEvent(config_id=uuid.uuid4())
+    with pytest.raises(GXCoreError) as e:
+        action.run(event=event, id=str(uuid.uuid4()))
+
+    assert e.value.error_code == expected_error_code
+    assert e.value.get_error_params() == {}
+
+
+def test_test_draft_datasource_config_raises_for_unknown_type(
+    mock_context, mocker: MockerFixture, set_required_env_vars: None
+):
+    datasource_config = {"type": "not a datasource", "name": "test-1-2-3"}
+    config_id = UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5")
+    create_session = mocker.patch(
+        "great_expectations_cloud.agent.actions.draft_datasource_config_action.create_session"
+    )
+    session = create_session.return_value
+    response = session.get.return_value
+    response.ok = True
+    response.json.return_value = build_payload(config=datasource_config, id=config_id)
+    env_vars = GxAgentEnvVars()
+    action = DraftDatasourceConfigAction(context=mock_context)
+    job_id = UUID("87657a8e-f65e-4e64-b21f-e83a54738b75")
+    event = DraftDatasourceConfigEvent(config_id=config_id)
+    expected_url = (
+        f"{env_vars.gx_cloud_base_url}/organizations/{env_vars.gx_cloud_organization_id}"
+        f"/datasources/drafts/{config_id}"
+    )
+
+    mock_context.sources.type_lookup = {}
+
+    with pytest.raises(TypeError, match="unknown Data Source type"):
+        action.run(event=event, id=str(job_id))
+
+    session.get.assert_called_with(expected_url)
+
+
+def test_test_draft_datasource_config_raises_for_cloud_backend_error(
+    mock_context, mocker: MockerFixture, set_required_env_vars: None
+):
+    datasource_config = {"type": "not a datasource", "name": "test-1-2-3"}
+    config_id = UUID("df02b47c-e1b8-48a8-9aaa-b6ed9c49ffa5")
+    create_session = mocker.patch(
+        "great_expectations_cloud.agent.actions.draft_datasource_config_action.create_session"
+    )
+    session = create_session.return_value
+    response = session.get.return_value
+    response.ok = False
+    response.json.return_value = build_payload(config=datasource_config, id=config_id)
+    env_vars = GxAgentEnvVars()
+    action = DraftDatasourceConfigAction(context=mock_context)
+    job_id = UUID("87657a8e-f65e-4e64-b21f-e83a54738b75")
+    event = DraftDatasourceConfigEvent(config_id=config_id)
+    expected_url = (
+        f"{env_vars.gx_cloud_base_url}/organizations/{env_vars.gx_cloud_organization_id}"
+        f"/datasources/drafts/{config_id}"
+    )
+
+    with pytest.raises(RuntimeError, match="error while connecting to GX Cloud"):
+        action.run(event=event, id=str(job_id))
+
+    session.get.assert_called_with(expected_url)
---
+++ tests/agent/test_agent.py
@@ -0,0 +1,393 @@
+from __future__ import annotations
+
+import uuid
+from time import sleep
+from typing import TYPE_CHECKING, Callable, Literal
+from unittest.mock import call
+
+import pytest
+import responses
+
+from great_expectations_cloud.agent import GXAgent
+from great_expectations_cloud.agent.actions.agent_action import ActionResult
+from great_expectations_cloud.agent.agent import GXAgentConfig, GXAgentConfigError
+from great_expectations_cloud.agent.constants import USER_AGENT_HEADER, HeaderName
+from great_expectations_cloud.agent.message_service.asyncio_rabbit_mq_client import (
+    ClientError,
+)
+from great_expectations_cloud.agent.message_service.subscriber import (
+    EventContext,
+    SubscriberError,
+)
+from great_expectations_cloud.agent.models import (
+    DraftDatasourceConfigEvent,
+    JobCompleted,
+    JobStarted,
+    RunCheckpointEvent,
+    RunOnboardingDataAssistantEvent,
+)
+from tests.agent.conftest import FakeSubscriber
+
+if TYPE_CHECKING:
+    from tests.agent.conftest import DataContextConfigTD
+
+
+@pytest.fixture
+def set_required_env_vars(monkeypatch, org_id, token):
+    monkeypatch.setenv("GX_CLOUD_ORGANIZATION_ID", org_id)
+    monkeypatch.setenv("GX_CLOUD_ACCESS_TOKEN", token)
+
+
+@pytest.fixture
+def set_required_env_vars_missing_token(monkeypatch, org_id):
+    monkeypatch.setenv("GX_CLOUD_ORGANIZATION_ID", org_id)
+
+
+@pytest.fixture
+def set_required_env_vars_missing_org_id(monkeypatch, token):
+    monkeypatch.setenv("GX_CLOUD_ACCESS_TOKEN", token)
+
+
+@pytest.fixture
+def gx_agent_config(
+    set_required_env_vars, queue, connection_string, org_id, token
+) -> GXAgentConfig:
+    config = GXAgentConfig(
+        queue=queue,
+        connection_string=connection_string,
+        gx_cloud_access_token=token,
+        gx_cloud_organization_id=org_id,
+    )
+    return config
+
+
+@pytest.fixture
+def gx_agent_config_missing_token(
+    set_required_env_vars_missing_token, queue, connection_string, org_id, token
+) -> GXAgentConfig:
+    config = GXAgentConfig(
+        queue=queue,
+        connection_string=connection_string,
+        gx_cloud_access_token=token,
+        gx_cloud_organization_id=org_id,
+    )
+    return config
+
+
+@pytest.fixture
+def gx_agent_config_missing_org_id(
+    set_required_env_vars_missing_org_id, queue, connection_string, org_id, token
+) -> GXAgentConfig:
+    config = GXAgentConfig(
+        queue=queue,
+        connection_string=connection_string,
+        gx_cloud_access_token=token,
+        gx_cloud_organization_id=org_id,
+    )
+    return config
+
+
+@pytest.fixture
+def org_id():
+    return "4ea2985c-4fb7-4c53-9f8e-07b7e0506c3e"
+
+
+@pytest.fixture
+def token():
+    return "MTg0NDkyYmYtNTBiOS00ZDc1LTk3MmMtYjQ0M2NhZDA2NjJk"
+
+
+@pytest.fixture
+def get_context(mocker):
+    get_context = mocker.patch("great_expectations_cloud.agent.agent.get_context")
+    return get_context
+
+
+@pytest.fixture
+def client(mocker):
+    """Patch for agent.RabbitMQClient"""
+    client = mocker.patch("great_expectations_cloud.agent.agent.AsyncRabbitMQClient")
+    return client
+
+
+@pytest.fixture
+def subscriber(mocker):
+    """Patch for agent.Subscriber"""
+    subscriber = mocker.patch("great_expectations_cloud.agent.agent.Subscriber")
+    return subscriber
+
+
+@pytest.fixture
+def event_handler(mocker):
+    event_handler = mocker.patch("great_expectations_cloud.agent.agent.EventHandler")
+    return event_handler
+
+
+@pytest.fixture
+def queue():
+    return "3ee9791c-4ea6-479d-9b05-98217e70d341"
+
+
+@pytest.fixture
+def connection_string():
+    return "amqps://user:pass@great_expectations.io:5671"
+
+
+@pytest.fixture(autouse=True)
+def create_session(mocker, queue, connection_string):
+    """Patch for great_expectations.core.http.create_session"""
+    create_session = mocker.patch("great_expectations_cloud.agent.agent.create_session")
+    create_session().post().json.return_value = {
+        "queue": queue,
+        "connection_string": connection_string,
+    }
+    create_session().post().ok = True
+    return create_session
+
+
+def test_gx_agent_gets_env_vars_on_init(get_context, gx_agent_config):
+    agent = GXAgent()
+    assert agent._config == gx_agent_config
+
+
+def test_gx_agent_initializes_cloud_context(get_context, gx_agent_config):
+    GXAgent()
+    get_context.assert_called_with(cloud_mode=True)
+
+
+def test_gx_agent_run_starts_subscriber(get_context, subscriber, client, gx_agent_config):
+    """Expect GXAgent.run to invoke the Subscriber class with the correct arguments."""
+    agent = GXAgent()
+    agent.run()
+
+    subscriber.assert_called_with(client=client())
+
+
+def test_gx_agent_run_invokes_consume(get_context, subscriber, client, gx_agent_config):
+    """Expect GXAgent.run to invoke subscriber.consume with the correct arguments."""
+    agent = GXAgent()
+    agent.run()
+
+    subscriber().consume.assert_called_with(
+        queue=gx_agent_config.queue,
+        on_message=agent._handle_event_as_thread_enter,
+    )
+
+
+def test_gx_agent_run_closes_subscriber(get_context, subscriber, client, gx_agent_config):
+    """Expect GXAgent.run to invoke subscriber.close."""
+    agent = GXAgent()
+    agent.run()
+
+    subscriber().close.assert_called_with()
+
+
+def test_gx_agent_run_handles_client_error_on_init(
+    get_context, subscriber, client, gx_agent_config
+):
+    client.side_effect = ClientError
+    agent = GXAgent()
+    agent.run()
+
+
+def test_gx_agent_run_handles_subscriber_error_on_init(
+    get_context, subscriber, client, gx_agent_config
+):
+    subscriber.side_effect = SubscriberError
+    agent = GXAgent()
+    agent.run()
+
+
+def test_gx_agent_run_handles_subscriber_error_on_consume(
+    get_context, subscriber, client, gx_agent_config
+):
+    subscriber.consume.side_effect = SubscriberError
+    agent = GXAgent()
+    agent.run()
+
+
+def test_gx_agent_run_handles_subscriber_error_on_close(
+    get_context, subscriber, client, gx_agent_config
+):
+    subscriber.close.side_effect = SubscriberError
+    agent = GXAgent()
+    agent.run()
+
+
+def test_gx_agent_updates_cloud_on_job_status(
+    subscriber, create_session, get_context, client, gx_agent_config, event_handler
+):
+    correlation_id = "4ae63677-4dd5-4fb0-b511-870e7a286e77"
+    url = (
+        f"{gx_agent_config.gx_cloud_base_url}/organizations/"
+        f"{gx_agent_config.gx_cloud_organization_id}/agent-jobs/{correlation_id}"
+    )
+    job_started_data = JobStarted().json()
+    job_completed = JobCompleted(success=True, created_resources=[])
+    job_completed_data = job_completed.json()
+
+    async def redeliver_message():
+        return None
+
+    event = RunOnboardingDataAssistantEvent(datasource_name="test-ds", data_asset_name="test-da")
+
+    end_test = False
+
+    def signal_subtask_finished():
+        nonlocal end_test
+        end_test = True
+
+    event_context = EventContext(
+        event=event,
+        correlation_id=correlation_id,
+        processed_successfully=signal_subtask_finished,
+        processed_with_failures=signal_subtask_finished,
+        redeliver_message=redeliver_message,
+    )
+    event_handler.return_value.handle_event.return_value = ActionResult(
+        id=correlation_id, type=event.type, created_resources=[]
+    )
+
+    def consume(queue: str, on_message: Callable[[EventContext], None]):
+        """util to allow testing agent behavior without a subscriber.
+
+        Replicates behavior of Subscriber.consume by invoking the on_message
+        parameter with an event_context.
+        """
+        nonlocal event_context
+        on_message(event_context)
+
+        # we need the main thread to remain alive until event handler has finished
+        nonlocal end_test
+        while end_test is False:
+            sleep(0)  # defer control
+
+    subscriber().consume = consume
+
+    agent = GXAgent()
+    agent.run()
+
+    create_session.return_value.patch.assert_has_calls(
+        calls=[
+            call(url, data=job_started_data),
+            call(url, data=job_completed_data),
+        ],
+    )
+
+
+def test_invalid_config_agent_missing_token(gx_agent_config_missing_token):
+    with pytest.raises(GXAgentConfigError):
+        GXAgent()
+
+
+def test_invalid_config_agent_missing_org_id(gx_agent_config_missing_org_id):
+    with pytest.raises(GXAgentConfigError):
+        GXAgent()
+
+
+def test_custom_user_agent(
+    mock_gx_version_check: None,
+    set_required_env_vars: None,
+    gx_agent_config: GXAgentConfig,
+):
+    """Ensure custom User-Agent header is set on GX Cloud api calls."""
+    base_url = gx_agent_config.gx_cloud_base_url
+    org_id = gx_agent_config.gx_cloud_organization_id
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            responses.GET,
+            f"{base_url}/organizations/{org_id}/data-context-configuration",
+            json={
+                "anonymous_usage_statistics": {
+                    "data_context_id": str(uuid.uuid4()),
+                    "enabled": False,
+                },
+                "datasources": {},
+                "stores": {},
+            },
+            # match will fail if the User-Agent header is not set
+            match=[
+                responses.matchers.header_matcher(
+                    {
+                        HeaderName.USER_AGENT: f"{USER_AGENT_HEADER}/{GXAgent.get_current_gx_agent_version()}"
+                    }
+                )
+            ],
+        )
+        GXAgent()
+
+
+@pytest.fixture
+def ds_config_factory() -> Callable[[str], dict[Literal["name", "type", "connection_string"], str]]:
+    """
+    Return a factory that takes a `name` and creates valid datasource config dicts.
+    The datasource will always be an in-memory sqlite datasource that will pass `.test_connection()`
+    But will fail if trying to add a TableAsset because no tables exist for it.
+    """
+
+    def _factory(name: str = "test-ds") -> dict[Literal["name", "type", "connection_string"], str]:
+        return {
+            "name": name,
+            "type": "sqlite",
+            "connection_string": "sqlite:///",
+        }
+
+    return _factory
+
+
+def test_correlation_id_header(
+    mock_gx_version_check: None,
+    set_required_env_vars: None,
+    data_context_config: DataContextConfigTD,
+    ds_config_factory: Callable[[str], dict[Literal["name", "type", "connection_string"], str]],
+    gx_agent_config: GXAgentConfig,
+    fake_subscriber: FakeSubscriber,
+):
+    """Ensure agent-job-id/correlation-id header is set on GX Cloud api calls and updated for every new job."""
+    agent_job_ids: list[str] = [str(uuid.uuid4()) for _ in range(3)]
+    datasource_config_id_1 = uuid.UUID("00000000-0000-0000-0000-000000000001")
+    datasource_config_id_2 = uuid.UUID("00000000-0000-0000-0000-000000000002")
+    checkpoint_id = uuid.UUID("00000000-0000-0000-0000-000000000003")
+    # seed the fake queue with an event that will be consumed by the agent
+    fake_subscriber.test_queue.extendleft(
+        [
+            (DraftDatasourceConfigEvent(config_id=datasource_config_id_1), agent_job_ids[0]),
+            (DraftDatasourceConfigEvent(config_id=datasource_config_id_2), agent_job_ids[1]),
+            (
+                RunCheckpointEvent(checkpoint_id=checkpoint_id, datasource_names_to_asset_names={}),
+                agent_job_ids[2],
+            ),
+        ]
+    )
+
+    base_url = gx_agent_config.gx_cloud_base_url
+    org_id = gx_agent_config.gx_cloud_organization_id
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            responses.GET,
+            f"{base_url}/organizations/{org_id}/data-context-configuration",
+            json=data_context_config,
+        )
+        rsps.add(
+            responses.GET,
+            f"{base_url}/organizations/{org_id}/datasources/drafts/{datasource_config_id_1}",
+            json={"data": {"attributes": {"draft_config": ds_config_factory("test-ds-1")}}},
+            # match will fail if correlation-id header is not set
+            match=[responses.matchers.header_matcher({HeaderName.AGENT_JOB_ID: agent_job_ids[0]})],
+        )
+        rsps.add(
+            responses.GET,
+            f"{base_url}/organizations/{org_id}/datasources/drafts/{datasource_config_id_2}",
+            json={"data": {"attributes": {"draft_config": ds_config_factory("test-ds-2")}}},
+            # match will fail if correlation-id header is not set
+            match=[responses.matchers.header_matcher({HeaderName.AGENT_JOB_ID: agent_job_ids[1]})],
+        )
+        rsps.add(
+            responses.GET,
+            f"{base_url}organizations/{org_id}/checkpoints/{checkpoint_id}",
+            json={"data": {}},
+            # match will fail if correlation-id header is not set
+            match=[responses.matchers.header_matcher({HeaderName.AGENT_JOB_ID: agent_job_ids[2]})],
+        )
+        agent = GXAgent()
+        agent.run()
