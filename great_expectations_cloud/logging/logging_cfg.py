5418602f6f8c47ad9b9b221d99029f41eda6fc2b
Robby Reinold <robby@greatexpectations.io>
2024-04-09 01:07:32 -0400

    refactor

--- great_expectations_cloud/logging/logging_cfg.py
+++ great_expectations_cloud/logging/logging_cfg.py
@@ -6,13 +6,17 @@ import logging
 import logging.config
 import logging.handlers
 import pathlib
-from typing import MutableMapping, Any, Sequence
+from typing import TYPE_CHECKING, Any, Final, MutableMapping, Sequence
 
 import structlog
-from structlog.typing import Processor
 from typing_extensions import override
 
+if TYPE_CHECKING:
+    from structlog.typing import Processor
+
 LOGGER = logging.getLogger(__name__)
+SERVICE_NAME: Final[str] = "gx-agent"
+DEFAULT_FILE_LOGGING_LEVEL: Final[int] = logging.DEBUG
 
 
 class LogLevel(str, enum.Enum):
@@ -42,7 +46,11 @@ class LogLevel(str, enum.Enum):
 
 
 def configure_logger(
-    log_level: LogLevel, skip_log_file: bool, log_cfg_file: pathlib.Path | None, json_log: bool, environment: str
+    log_level: LogLevel,
+    skip_log_file: bool,
+    json_log: bool,
+    environment: str,
+    log_cfg_file: pathlib.Path | None,
 ) -> None:
     """
     Configure the root logger for the application.
@@ -54,63 +62,79 @@ def configure_logger(
     Note: this method should only be called once in the lifecycle of the application.
     """
     if log_cfg_file:
-        if not log_cfg_file.exists():
-            raise FileNotFoundError(  # noqa: TRY003 # one off error
-                f"Logging config file not found: {log_cfg_file.absolute()}"
-            )
-        dict_config = json.loads(log_cfg_file.read_text())
-        logging.config.dictConfig(dict_config)
-        LOGGER.info(f"Configured logging from file {log_cfg_file}")
+        _load_cfg_from_file(log_cfg_file)
         return
+
+    root = logging.getLogger()
+    root.setLevel(log_level.numeric_level)
+
+    structured_log_handler = _get_structured_log_handler(json_log, environment)
+    root.addHandler(structured_log_handler)
+
+    if not skip_log_file:
+        file_handler = _get_file_handler()
+        root.addHandler(file_handler)
+
+
+def _get_file_handler():
+    formatter = logging.Formatter(
+        "%(asctime)s | %(name)s | line: %(lineno)d | %(levelname)s: %(message)s"
+    )
+    log_dir = pathlib.Path("logs")
+    if not log_dir.exists():
+        pathlib.Path(log_dir).mkdir()
+    # The FileHandler writes all logs to a local file
+    file_handler = logging.handlers.TimedRotatingFileHandler(
+        filename=log_dir / "logfile", when="midnight", backupCount=30
+    )  # creates a new file every day; keeps 30 days of logs at most
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(DEFAULT_FILE_LOGGING_LEVEL)
+    file_handler.namer = lambda name: name + ".log"  # append file extension to name
+    return file_handler
+
+
+def _get_structured_log_handler(json_log: bool, environment: str):
     def _add_our_custom_fields(
-            logger: structlog.types.WrappedLogger,
-            method_name: str,
-            event_dict: MutableMapping[str, Any],
+        logger: structlog.types.WrappedLogger,
+        method_name: str,
+        event_dict: MutableMapping[str, Any],
     ) -> MutableMapping[str, Any]:
-        event_dict["service"] = "gx-agent"
+        event_dict["service"] = SERVICE_NAME
         event_dict["env"] = environment
 
         return event_dict
 
-    custom_fmt_processors: list[Processor]  = [_add_our_custom_fields]
+    handler = logging.StreamHandler()
+
+    custom_fmt_processors: list[Processor] = [_add_our_custom_fields]
 
     formatter = structlog.stdlib.ProcessorFormatter(
-        processors=_build_processors(json_log),
         foreign_pre_chain=_build_pre_processors(custom_fmt_processors),
+        processors=_select_final_output(json_log),
     )
-    handler = logging.StreamHandler()
     handler.setFormatter(formatter)
-    root = logging.getLogger()
-    root.setLevel(log_level.numeric_level)
-    root.addHandler(handler)
-
-    if skip_log_file:
-        return
-
-    logDirectory = pathlib.Path("logs")
-    if not logDirectory.exists():
-        pathlib.Path(logDirectory).mkdir()
-
-    # The FileHandler writes all logs to a local file
-    file_handler = logging.handlers.TimedRotatingFileHandler(
-        filename=logDirectory / "logfile", when="midnight", backupCount=30
-    )  # creates a new file every day; keeps 30 days of logs at most
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.namer = lambda name: name + ".log"  # append file extension to name
-    root.addHandler(file_handler)
+    return handler
 
 
-# This is mocked in our testing, so its useful to be in its own function
-def _build_processors(json_log:bool) -> Sequence[Processor]:
-    result: list[Processor] = [
-        structlog.stdlib.ProcessorFormatter.remove_processors_meta
-    ]
+def _load_cfg_from_file(log_cfg_file):
+    if not log_cfg_file.exists():
+        raise FileNotFoundError(  # noqa: TRY003 # one off error
+            f"Logging config file not found: {log_cfg_file.absolute()}"
+        )
+    dict_config = json.loads(log_cfg_file.read_text())
+    logging.config.dictConfig(dict_config)
+    LOGGER.info(f"Configured logging from file {log_cfg_file}")
+
+
+def _select_final_output(json_log: bool) -> Sequence[Processor]:
+    # remove_processors_meta cleans up the event dict
+    processors: list[Processor] = [structlog.stdlib.ProcessorFormatter.remove_processors_meta]
     if json_log:
-        result.append(structlog.processors.JSONRenderer())
+        renderer = structlog.processors.JSONRenderer()
     else:
-        result.append(structlog.dev.ConsoleRenderer())
-    return result
+        renderer = structlog.dev.ConsoleRenderer()
+    processors.append(renderer)
+    return processors
 
 
 # Wow the order of these processors is important
@@ -137,7 +161,6 @@ def _build_pre_processors(custom_processors: list[Processor]):
         # Convert all key values to unicode format
         structlog.processors.UnicodeDecoder(),
         # adds our custom environment vars
-        #   .warn("something bad", ..., service="mercury", logging_version="0.0.1", env="dev")
-        *custom_processors
+        #   .warn("something bad", ..., service="gx-agent", env="production")
+        *custom_processors,
     ]
-
