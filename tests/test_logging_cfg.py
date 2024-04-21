1274b77d57eca83abf02cd9e28dd0554b6a55f56
Robby Reinold <robby@greatexpectations.io>
2024-04-21 12:37:30 -0400

    add fmt tests

--- tests/test_logging_cfg.py
+++ tests/test_logging_cfg.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
 
+import json
+
+import freezegun
 import pytest
 
-from great_expectations_cloud.logging.logging_cfg import LogLevel
+from great_expectations_cloud.logging.logging_cfg import JSONFormatter, LogLevel
+
+TIMESTAMP = "2024-01-01T00:00:00+00:00"
+
+# JSON_FIELDS = (
 
 
 class TestLogLevel:
@@ -35,6 +42,39 @@ class TestLogLevel:
         assert LogLevel.WARNING.numeric_level == 30
         assert LogLevel.ERROR.numeric_level == 40
         assert LogLevel.CRITICAL.numeric_level == 50
+
+
+import logging
+from unittest import TestCase
+
+from great_expectations_cloud.logging.logging_cfg import configure_logger
+
+
+class TestLogging(TestCase):
+    def test_logging(self):
+        configure_logger(LogLevel.INFO, False, True, {}, None)
+        logger = logging.getLogger()
+        with self.assertLogs(logger, level="INFO") as cm:
+            logger.info("first message")
+            logger.error("second mesage")
+            # [ERROR] great_expectations_cloud.agent.agent: GX Agent version: 0.0.47.dev0
+            self.assertEqual(cm.output, ["INFO:foo:first message", "ERROR:foo.bar:second message"])
+
+
+@pytest.mark.parametrize("custom_tags", [{}, {"environment": "jungle"}])
+@freezegun.freeze_time(TIMESTAMP)
+def test_json_formatter(custom_tags):
+    default_log_json = {
+        "event": "hello",
+        "level": "Level None",
+        "logger": None,
+        "timestamp": TIMESTAMP,
+    }
+    expected = {**default_log_json, **custom_tags}
+    fmt = JSONFormatter(custom_tags)
+    out_str = fmt.format(logging.makeLogRecord({"msg": "hello"}))
+    actual = json.loads(out_str)
+    assert actual == expected
 
 
 if __name__ == "__main__":
