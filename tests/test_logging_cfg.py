7e80099b4fc0daafca54a9ec9d76bc9e26af5505
Robby Reinold <robby@greatexpectations.io>
2024-04-21 23:49:45 -0400

    cover fs

--- tests/test_logging_cfg.py
+++ tests/test_logging_cfg.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
+
+import uuid
 from logging import makeLogRecord
 import json
+from pathlib import Path
 
 import freezegun
 import pytest
 
-from great_expectations_cloud.logging.logging_cfg import JSONFormatter, LogLevel
+from great_expectations_cloud.logging.logging_cfg import JSONFormatter, LogLevel, DEFAULT_LOG_DIR, DEFAULT_LOG_FILE
 
 TIMESTAMP = "2024-01-01T00:00:00+00:00"
 
@@ -20,6 +23,9 @@ default_log_emitted = {"msg": "hello", "name": "root", "levelname": "DEBUG"}
 
 # JSON_FIELDS = (
 
+@pytest.fixture
+def logfile_path():
+    return Path(DEFAULT_LOG_DIR, DEFAULT_LOG_FILE)
 
 class TestLogLevel:
     @pytest.mark.parametrize(
@@ -91,6 +97,41 @@ def test_json_formatter_stack_info():
     actual = json.loads(out_str)
     assert actual == expected
 
+
+import os
+
+
+def test_logfile(fs, logfile_path):
+    """
+    fs sets up a fake fs
+    """
+    assert not os.path.exists(logfile_path)
+    configure_logger(LogLevel.DEBUG, False, False, {}, None)
+    id_in_log = str(uuid.uuid4())
+    logging.getLogger().fatal(id_in_log)
+    assert os.path.exists(logfile_path)
+    with open(logfile_path, 'r') as f:
+        assert id_in_log in f.read()
+
+def test_logfile_skip_log_file(fs, logfile_path):
+    """
+    fs sets up a fake fs
+    """
+    assert not os.path.exists(logfile_path)
+    disable_log_file = True
+    configure_logger(LogLevel.DEBUG, disable_log_file, False, {}, None)
+    logging.getLogger().fatal("very bad")
+    assert not os.path.exists(logfile_path)
+
+def test_logger_json():
+    """
+    fs sets up a fake fs
+    """
+    json_log = True
+    logger = configure_logger(LogLevel.DEBUG, False, json_log, {}, None)
+    handlers = logging.getLogger().handlers
+    formatter_types = [ type(x.formatter) for x in handlers]
+    assert JSONFormatter in formatter_types
 
 if __name__ == "__main__":
     pytest.main([__file__, "-vv"])
