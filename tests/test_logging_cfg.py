9118dab7fc741bc691b0295cdcd464153c2414ad
Robby Reinold <robby@greatexpectations.io>
2024-04-22 00:10:49 -0400

    add file missing test

--- tests/test_logging_cfg.py
+++ tests/test_logging_cfg.py
@@ -1,31 +1,38 @@
 from __future__ import annotations
 
+import json
 import uuid
 from logging import makeLogRecord
-import json
 from pathlib import Path
 
 import freezegun
 import pytest
 
-from great_expectations_cloud.logging.logging_cfg import JSONFormatter, LogLevel, DEFAULT_LOG_DIR, DEFAULT_LOG_FILE
+from great_expectations_cloud.logging.logging_cfg import (
+    DEFAULT_LOG_DIR,
+    DEFAULT_LOG_FILE,
+    JSONFormatter,
+    LogLevel,
+)
 
 TIMESTAMP = "2024-01-01T00:00:00+00:00"
 
 default_log_formatted = {
-        "event": "hello",
-        "level": "DEBUG",
-        "logger": 'root',
-        "timestamp": TIMESTAMP,
-    }
+    "event": "hello",
+    "level": "DEBUG",
+    "logger": "root",
+    "timestamp": TIMESTAMP,
+}
 
 default_log_emitted = {"msg": "hello", "name": "root", "levelname": "DEBUG"}
 
 # JSON_FIELDS = (
 
+
 @pytest.fixture
 def logfile_path():
     return Path(DEFAULT_LOG_DIR, DEFAULT_LOG_FILE)
+
 
 class TestLogLevel:
     @pytest.mark.parametrize(
@@ -60,10 +67,8 @@ class TestLogLevel:
 
 
 import logging
-from unittest import TestCase
 
 from great_expectations_cloud.logging.logging_cfg import configure_logger
-
 
 
 @pytest.mark.parametrize("custom_tags", [{}, {"environment": "jungle"}])
@@ -77,22 +82,22 @@ def test_json_formatter(custom_tags):
 
     assert actual == expected
 
+
 @freezegun.freeze_time(TIMESTAMP)
 def test_json_formatter_exc_info():
-
-    expected = {**default_log_formatted, 'exc_info': '(1, 2, 3)'}
+    expected = {**default_log_formatted, "exc_info": "(1, 2, 3)"}
     fmt = JSONFormatter()
     log_record = makeLogRecord({**default_log_emitted, "exc_info": (1, 2, 3)})
     out_str = fmt.format(log_record)
     actual = json.loads(out_str)
     assert actual == expected
 
+
 @freezegun.freeze_time(TIMESTAMP)
 def test_json_formatter_stack_info():
-
-    expected = {**default_log_formatted, 'stack_info': 'what is this?'}
+    expected = {**default_log_formatted, "stack_info": "what is this?"}
     fmt = JSONFormatter()
-    log_record = makeLogRecord({**default_log_emitted, 'stack_info': 'what is this?'})
+    log_record = makeLogRecord({**default_log_emitted, "stack_info": "what is this?"})
     out_str = fmt.format(log_record)
     actual = json.loads(out_str)
     assert actual == expected
@@ -110,8 +115,9 @@ def test_logfile(fs, logfile_path):
     id_in_log = str(uuid.uuid4())
     logging.getLogger().fatal(id_in_log)
     assert os.path.exists(logfile_path)
-    with open(logfile_path, 'r') as f:
+    with open(logfile_path) as f:
         assert id_in_log in f.read()
+
 
 def test_logfile_skip_log_file(fs, logfile_path):
     """
@@ -123,15 +129,53 @@ def test_logfile_skip_log_file(fs, logfile_path):
     logging.getLogger().fatal("very bad")
     assert not os.path.exists(logfile_path)
 
+
 def test_logger_json():
     """
     fs sets up a fake fs
     """
     json_log = True
-    logger = configure_logger(LogLevel.DEBUG, False, json_log, {}, None)
+    configure_logger(LogLevel.DEBUG, False, json_log, {}, None)
     handlers = logging.getLogger().handlers
-    formatter_types = [ type(x.formatter) for x in handlers]
+    formatter_types = [type(x.formatter) for x in handlers]
     assert JSONFormatter in formatter_types
+
+
+# Re enable fs fake
+def test_load_logging_cfg():
+    config_dict = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {"default_fmt": {"format": "[%(levelname)s] %(name)s: %(message)s"}},
+        "handlers": {
+            "config_handler": {
+                "formatter": "default_fmt",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["config_handler"],
+                "level": "WARNING",
+                "propagate": False,
+            },
+        },
+    }
+    config_str = json.dumps(config_dict)
+    config_path = "log.config"
+    with open(config_path, "wb") as f:
+        f.write(config_str.encode())
+    assert os.path.exists(config_path)
+    configure_logger(LogLevel.DEBUG, False, False, {}, Path(config_path))
+    handlers = logging.getLogger().handlers
+    assert "config_handler" in [x.name for x in handlers]
+
+
+def test_log_cfg_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        configure_logger(LogLevel.DEBUG, False, False, {}, Path("nowhere"))
+
 
 if __name__ == "__main__":
     pytest.main([__file__, "-vv"])
