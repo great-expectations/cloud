78e49c523928d8208b3bcdca9d9cd6ea74a9ea5e
William Shin <will@greatexpectations.io>
2024-04-25 18:40:20 -0700

    push

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -14,7 +14,7 @@ LOGGER: Final = logging.getLogger("tests")
 @pytest.fixture(scope="module")
 def context() -> CloudDataContext:
     context = gx.get_context(
-        cloud_mode=True,
+        mode="cloud",
         cloud_base_url=os.environ.get("GX_CLOUD_BASE_URL"),
         cloud_organization_id=os.environ.get("GX_CLOUD_ORGANIZATION_ID"),
         cloud_access_token=os.environ.get("GX_CLOUD_ACCESS_TOKEN"),
