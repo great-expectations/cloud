ecbc6ef8762993f1e8da69e1d8cdb65d9d65cc9b
William Shin <will@greatexpectations.io>
2024-05-01 17:10:07 -0700

    beam me up

--- tests/integration/actions/test_metric_action.py
+++ tests/integration/actions/test_metric_action.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import json
+import os
+from typing import TYPE_CHECKING
 
 import pytest
 import requests
@@ -11,11 +13,15 @@ from great_expectations_cloud.agent.models import (
     RunMetricsListEvent,
 )
 
+if TYPE_CHECKING:
+    from great_expectations.data_context import CloudDataContext
+    from great_expectations.datasource.fluent import PostgresDatasource
+    from great_expectations.datasource.fluent.sql_datasource import TableAsset
+
 
 @pytest.fixture
-def user_api_token_headers_org_admin_sc_org():
-    # api_token: str = "d47995c6872540429c98f94d6182b5c4.V1.6CmUs5x39flxrKpltY9PYRYiN8DUjNy8vZSaB70nztPpWMg-80LeyjSl-yQvDnxnoNwJeXlBc-fB3HhosVPa7A"
-    api_token: str = "808d6f9e99c94caaaf70baac1a4581ee.V1.kPujCIKZZDLdhw8xUuTb8ZX7oIRhIQY0MgrxRS8sPlX6Dp8121jSTcMlmPwhIMgciNIqkXrTMkgZ5invhUfuFg"
+def user_api_token_headers_org_admin_sc_org() -> dict:
+    api_token: str = os.environ.get("GX_CLOUD_ACCESS_TOKEN")
     return {
         "Authorization": f"Bearer {api_token}",
         "Content-Type": "application/vnd.api+json",
@@ -24,6 +30,7 @@ def user_api_token_headers_org_admin_sc_org():
 
 @pytest.fixture
 def query():
+    # only the table metrics
     return """
         query Asset($assetId: UUID!) {
             dataAsset(id: $assetId) {
@@ -39,34 +46,9 @@ def query():
                     metrics {
                         columnName
                         columnDataType
-                        valueRangeMin
-                        valueRangeMax
-                        valueRangeMinUnion {
-                            __typename
-                            ...MetricValueStringTypeFragment
-                            ...MetricValueFloatTypeFragment
-                        }
-                        valueRangeMaxUnion {
-                            __typename
-                            ...MetricValueStringTypeFragment
-                            ...MetricValueFloatTypeFragment
-                        }
-                        mean
-                        median
-                        nullCount
                     }
                 }
             }
-        }
-
-        fragment MetricValueStringTypeFragment on MetricValueStringType {
-            __typename
-            stringValue
-        }
-
-        fragment MetricValueFloatTypeFragment on MetricValueFloatType {
-            __typename
-            floatValue
         }
     """
 
@@ -105,58 +87,62 @@ def graphql_test_client(
 
 
 @pytest.fixture
-def metric_list_event(datasource, data_asset):
-    return RunMetricsListEvent(
-        type="metrics_list_request.received",
-        datasource_name=datasource.name,
-        data_asset_name=data_asset.name,
-        metric_names=[MetricTypes.TABLE_COLUMN_TYPES, MetricTypes.TABLE_COLUMNS],
-    )
+def local_mercury_db_datasource(
+    context: CloudDataContext,
+) -> PostgresDatasource:
+    datasource_name = "local_mercury_db"
+    datasource = context.get_datasource(datasource_name=datasource_name)
+    yield datasource
+
+
+@pytest.fixture
+def local_mercury_db_organizations_table_asset(
+    local_mercury_db_datasource: PostgresDatasource,
+) -> TableAsset:
+    data_asset_name = "local-mercury-db-organizations-table"
+    data_asset = local_mercury_db_datasource.get_asset(asset_name=data_asset_name)
+    yield data_asset
 
 
 def test_running_metric_list_action(
-    context,
-    metric_list_event,
-    datasource,
-    data_asset,
-    batch_request,
-    cloud_base_url,
-    org_id,
-    token,
+    context: CloudDataContext,
     graphql_test_client,
-    user_api_token_headers_org_admin_sc_org,
-    query,
+    user_api_token_headers_org_admin_sc_org: dict,
+    query: str,
+    local_mercury_db_datasource: PostgresDatasource,
+    local_mercury_db_organizations_table_asset: TableAsset,
 ):
+    metrics_list_event = RunMetricsListEvent(
+        type="metrics_list_request.received",
+        datasource_name=local_mercury_db_datasource.name,
+        data_asset_name=local_mercury_db_organizations_table_asset.name,
+        metric_names=[
+            MetricTypes.TABLE_COLUMN_TYPES,
+            MetricTypes.TABLE_COLUMNS,
+            MetricTypes.TABLE_ROW_COUNT,
+        ],
+    )
+
     action = MetricListAction(context=context)
     event_id = "096ce840-7aa8-45d1-9e64-2833948f4ae8"
     action.run(
-        event=RunMetricsListEvent(
-            type="metrics_list_request.received",
-            datasource_name=datasource.name,
-            data_asset_name=data_asset.name,
-            metric_names=[
-                MetricTypes.TABLE_COLUMN_TYPES,
-                MetricTypes.TABLE_COLUMNS,
-                MetricTypes.TABLE_ROW_COUNT,
-                MetricTypes.COLUMN_NULL_COUNT,
-                MetricTypes.COLUMN_MAX,
-                MetricTypes.COLUMN_MIN,
-                MetricTypes.COLUMN_MEAN,
-                MetricTypes.COLUMN_MEDIAN,
-            ],
-        ),
+        event=metrics_list_event,
         id="test-id",
     )
-    action_result = action.run(event=metric_list_event, id=event_id, batch_request=batch_request)
-    assert action_result.type == metric_list_event.type
+    action_result = action.run(event=metrics_list_event, id=event_id)
+    assert action_result.type == metrics_list_event.type
     assert action_result.id == event_id
-    print(user_api_token_headers_org_admin_sc_org)
     result = graphql_test_client.execute(
         query,
         headers=user_api_token_headers_org_admin_sc_org,
         variables={
-            "assetId": data_asset.name,
+            "assetId": f"{local_mercury_db_organizations_table_asset.id}",
         },
     )
-    # dataAsset doesn't have Metrics
-    assert result == {"data": {"dataAsset": None}}
+
+    assert result["data"]["dataAsset"]["id"] == f"{local_mercury_db_organizations_table_asset.id}"
+    assert result["data"]["dataAsset"]["name"] == local_mercury_db_organizations_table_asset.name
+    assert result["data"]["dataAsset"]["datasourceId"] == f"{local_mercury_db_datasource.id}"
+    # assert rowCount is computed and Metrics is non-empty
+    assert result["data"]["dataAsset"]["latestMetricRun"]["metrics"]
+    assert result["data"]["dataAsset"]["latestMetricRun"]["rowCount"] != 0
