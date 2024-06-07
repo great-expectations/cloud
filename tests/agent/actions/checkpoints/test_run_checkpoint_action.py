a3f9dcbc1eded0d4eb59f6b3ea3e23e3c79327c3
Anthony Burdi <anthony@greatexpectations.io>
2024-06-07 18:13:52 -0400

    Parametrizing tests

--- tests/agent/actions/checkpoints/test_run_checkpoint_action.py
+++ tests/agent/actions/checkpoints/test_run_checkpoint_action.py
@@ -23,71 +23,47 @@ if TYPE_CHECKING:
 pytestmark = pytest.mark.unit
 
 
-@pytest.fixture
-def checkpoint_id():
-    return "5f3814d6-a2e2-40f9-ba75-87ddf485c3a8"
-
-
-@pytest.fixture
-def datasource_names_to_asset_names():
-    return {"Data Source 1": {"Data Asset A", "Data Asset B"}}
-
-
-@pytest.fixture
-def checkpoint_event_without_splitter_options(checkpoint_id, datasource_names_to_asset_names):
-    return RunCheckpointEvent(
-        type="run_checkpoint_request",
-        checkpoint_id=checkpoint_id,
-        datasource_names_to_asset_names=datasource_names_to_asset_names,
-    )
-
-
-@pytest.fixture
-def checkpoint_event_with_splitter_options(checkpoint_id, datasource_names_to_asset_names):
-    return RunCheckpointEvent(
-        type="run_checkpoint_request",
-        checkpoint_id=checkpoint_id,
-        datasource_names_to_asset_names=datasource_names_to_asset_names,
-        splitter_options={"year": 2023, "month": 11, "day": 30},
-    )
-
-
-def test_run_checkpoint_action_without_splitter_options_returns_action_result(
-    mock_context, checkpoint_event_without_splitter_options, checkpoint_id
-):
-    action = RunCheckpointAction(context=mock_context)
-    id = "096ce840-7aa8-45d1-9e64-2833948f4ae8"
-    checkpoint = mock_context.run_checkpoint.return_value
-    checkpoint.ge_cloud_id = checkpoint_id
-    checkpoint.run_results = {
-        "GXCloudIdentifier::validation_result::78ebf58e-bdb5-4d79-88d5-79bae19bf7d0": {
-            "actions_results": {
-                "store_validation_result": {"id": "78ebf58e-bdb5-4d79-88d5-79bae19bf7d0"}
-            }
-        }
-    }
-    action_result = action.run(event=checkpoint_event_without_splitter_options, id=id)
-
-    mock_context.run_checkpoint.assert_called_with(
-        ge_cloud_id=UUID(checkpoint_id), batch_request=None
-    )
-    assert action_result.type == checkpoint_event_without_splitter_options.type
-    assert action_result.id == id
-    assert action_result.created_resources == [
-        CreatedResource(
-            resource_id="78ebf58e-bdb5-4d79-88d5-79bae19bf7d0",
-            type="SuiteValidationResult",
+@pytest.mark.parametrize(
+    "splitter_options, batch_request",
+    [
+        (
+            {"year": 2023, "month": 11, "day": 30},
+            {"options": {"day": 30, "month": 11, "year": 2023}},
         ),
-    ]
-
-
-def test_run_checkpoint_action_with_splitter_options_returns_action_result(
-    mock_context, checkpoint_event_with_splitter_options, checkpoint_id
+        ({}, None),
+        (None, None),
+    ],
+)
+@pytest.mark.parametrize(
+    "action_class,event",
+    [
+        (
+            RunCheckpointAction,
+            RunCheckpointEvent(
+                type="run_checkpoint_request",
+                datasource_names_to_asset_names={"Data Source 1": {"Data Asset A", "Data Asset B"}},
+                checkpoint_id="5f3814d6-a2e2-40f9-ba75-87ddf485c3a8",
+            ),
+        ),
+        (
+            RunScheduledCheckpointAction,
+            RunScheduledCheckpointEvent(
+                type="run_scheduled_checkpoint.received",
+                datasource_names_to_asset_names={"Data Source 1": {"Data Asset A", "Data Asset B"}},
+                checkpoint_id="5f3814d6-a2e2-40f9-ba75-87ddf485c3a8",
+                schedule_id=UUID("5f3814d6-a2e2-40f9-ba75-87ddf485c3a8"),
+            ),
+        ),
+    ],
+)
+def test_run_checkpoint_action_with_and_without_splitter_options_returns_action_result(
+    mock_context, action_class, event, splitter_options, batch_request
 ):
-    action = RunCheckpointAction(context=mock_context)
+    action = action_class(context=mock_context)
     id = "096ce840-7aa8-45d1-9e64-2833948f4ae8"
     checkpoint = mock_context.run_checkpoint.return_value
-    checkpoint.ge_cloud_id = checkpoint_id
+    checkpoint_id_str = "5f3814d6-a2e2-40f9-ba75-87ddf485c3a8"
+    checkpoint.ge_cloud_id = checkpoint_id_str
     checkpoint.run_results = {
         "GXCloudIdentifier::validation_result::78ebf58e-bdb5-4d79-88d5-79bae19bf7d0": {
             "actions_results": {
@@ -95,13 +71,13 @@ def test_run_checkpoint_action_with_splitter_options_returns_action_result(
             }
         }
     }
-    action_result = action.run(event=checkpoint_event_with_splitter_options, id=id)
+    event.splitter_options = splitter_options
+    action_result = action.run(event=event, id=id)
 
     mock_context.run_checkpoint.assert_called_with(
-        ge_cloud_id=UUID(checkpoint_id),
-        batch_request={"options": {"day": 30, "month": 11, "year": 2023}},
+        ge_cloud_id=UUID(checkpoint_id_str), batch_request=batch_request
     )
-    assert action_result.type == checkpoint_event_with_splitter_options.type
+    assert action_result.type == event.type
     assert action_result.id == id
     assert action_result.created_resources == [
         CreatedResource(
@@ -135,8 +111,6 @@ def test_run_checkpoint_action_with_splitter_options_returns_action_result(
 )
 def test_run_checkpoint_action_raises_on_test_connection_failure(
     mock_context,
-    checkpoint_id,
-    datasource_names_to_asset_names,
     mocker: MockerFixture,
     action_class,
     event,
