3ab757b56cbc720815db8e83f1788b976917d96c
Thu Pham <thu.pham@greatexpectations.io>
2024-09-06 07:40:20 -0700

    Update test_run.py

--- tests/agent/test_run.py
+++ tests/agent/test_run.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 from great_expectations_cloud.agent.run import run_agent
 
 
-def test_run_calls_gx_agent(mocker):
+def test_run_inits_gx_agent(mocker):
     agent = mocker.patch("great_expectations_cloud.agent.run.GXAgent")
+    asyncio = mocker.patch("great_expectations_cloud.agent.run.asyncio")
     run_agent()
-    agent.assert_called_with()
-    agent().run.assert_called_with()
+    agent.assert_called_once_with(config=mocker.ANY)  # gets passed the config
+    asyncio.run.assert_called_once_with(mocker.ANY)  # gets passed the app.run() call
