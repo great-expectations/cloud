9f5e81feda8638700a2dbe0a86e2a2756f084206
Anthony Burdi <anthony@greatexpectations.io>
2024-09-18 15:31:33 -0400

    Bump timeout

--- tests/agent/test_cli.py
+++ tests/agent/test_cli.py
@@ -15,7 +15,8 @@ import pytest
 def test_command_retuns_zero_exit_code(cmd: str):
     cli_cmds = ["gx-agent", cmd]
     print(f"Testing command:\n  {' '.join(cli_cmds)}\n")
-    cmplt_process = subprocess.run(cli_cmds, check=False, timeout=6.0)  # noqa: S603 # trusted input
+    # Note: 2024-09-18 Bumped timeout from 6->8 seconds to avoid flakiness on python 3.9
+    cmplt_process = subprocess.run(cli_cmds, check=False, timeout=8.0)  # noqa: S603 # trusted input
     print(cmplt_process.stdout)
     assert cmplt_process.returncode == 0
 
@@ -23,7 +24,8 @@ def test_command_retuns_zero_exit_code(cmd: str):
 def test_custom_log_tags_failure():
     cli_cmds = ["gx-agent", "--custom-log-tags", "{'badJSON"]
     print(f"Testing command:\n  {' '.join(cli_cmds)}\n")
-    cmplt_process = subprocess.run(cli_cmds, check=False, timeout=6.0)  # noqa: S603 # trusted input
+    # Note: 2024-09-18 Bumped timeout from 6->8 seconds to avoid flakiness on python 3.9
+    cmplt_process = subprocess.run(cli_cmds, check=False, timeout=8.0)  # noqa: S603 # trusted input
     print(cmplt_process.stdout)
     assert cmplt_process.returncode != 0
 
