c1db7c5d08474f9bcd805cf60ded6a71f6f7f02e
Anthony Burdi <anthony@greatexpectations.io>
2024-10-02 17:46:02 -0400

    WIP v0 version (no tests)

--- great_expectations_cloud/agent/actions/run_scheduled_checkpoint.py
+++ great_expectations_cloud/agent/actions/run_scheduled_checkpoint.py
@@ -6,7 +6,7 @@ from great_expectations_cloud.agent.actions.agent_action import (
     ActionResult,
     AgentAction,
 )
-from great_expectations_cloud.agent.actions.run_checkpoint import run_checkpoint
+from great_expectations_cloud.agent.actions.run_checkpoint import run_checkpoint_v0
 from great_expectations_cloud.agent.event_handler import register_event_action
 from great_expectations_cloud.agent.models import (
     RunScheduledCheckpointEvent,
@@ -19,7 +19,7 @@ class RunScheduledCheckpointAction(AgentAction[RunScheduledCheckpointEvent]):
 
     @override
     def run(self, event: RunScheduledCheckpointEvent, id: str) -> ActionResult:
-        return run_checkpoint(self._context, event, id)
+        return run_checkpoint_v0(self._context, event, id)
 
 
 register_event_action("0", RunScheduledCheckpointEvent, RunScheduledCheckpointAction)
--- great_expectations_cloud/agent/actions/run_window_checkpoint.py
+++ great_expectations_cloud/agent/actions/run_window_checkpoint.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+from great_expectations.core.http import create_session
+from great_expectations.exceptions import GXCloudError
 from typing_extensions import override
 
 from great_expectations_cloud.agent.actions.agent_action import (
     ActionResult,
     AgentAction,
 )
-from great_expectations_cloud.agent.actions.run_checkpoint import run_checkpoint
+from great_expectations_cloud.agent.actions.run_checkpoint import run_checkpoint_v0
 from great_expectations_cloud.agent.event_handler import register_event_action
 from great_expectations_cloud.agent.models import (
     RunWindowCheckpointEvent,
@@ -19,9 +21,30 @@ class RunWindowCheckpointAction(AgentAction[RunWindowCheckpointEvent]):
 
     @override
     def run(self, event: RunWindowCheckpointEvent, id: str) -> ActionResult:
-        # TODO: https://greatexpectations.atlassian.net/browse/ZELDA-922
-        #  This currently only runs a normal checkpoint. Logic for window checkpoints needs to be added (e.g. call the backend to get the params and then construct the evaluation_parameters before passing them into context.run_checkpoint()) One way we can do this via a param in `run_checkpoint()` that takes a function to build the evaluation_parameters, defaulting to a noop for the other checkpoint action types.
-        return run_checkpoint(self._context, event, id)
+        with create_session(access_token=self._auth_key) as session:
+            expectation_parameters_for_checkpoint_url = f"{self._base_url}api/v1/organizations/"
+            f"{self._organization_id}/checkpoints/{event.checkpoint_id}/expectation-parameters"
+            response = session.get(url=expectation_parameters_for_checkpoint_url)
+
+        if not response.ok:
+            raise GXCloudError(
+                message=f"RunWindowCheckpointAction encountered an error while connecting to GX Cloud. "
+                f"Unable to retrieve expectation_parameters for Checkpoint with ID={event.checkpoint_id}.",
+                response=response,
+            )
+        data = response.json()
+        try:
+            expectation_parameters = data["data"]["expectation_parameters"]
+        except KeyError as e:
+            raise GXCloudError(
+                message="Malformed response received from GX Cloud",
+                response=response,
+            ) from e
+
+        # Note: In v0 expectation_parameters are called evaluation_parameters.
+        return run_checkpoint_v0(
+            self._context, event, id, evaluation_parameters=expectation_parameters
+        )
 
 
 register_event_action("0", RunWindowCheckpointEvent, RunWindowCheckpointAction)
