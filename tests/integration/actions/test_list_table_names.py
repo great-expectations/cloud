a392f2cb4b5ccef11fda6f7e60b0959962e96102
T Pham <thuutrangpham@gmail.com>
2024-10-07 13:56:33 -0700

    List table names action v1 (#468)
    Co-authored-by: Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>

--- tests/integration/actions/test_list_table_names.py
+++ tests/integration/actions/test_list_table_names.py
@@ -15,7 +15,6 @@ if TYPE_CHECKING:
 pytestmark = pytest.mark.integration
 
 
-@pytest.mark.skip("Skipping integration tests until they are updated for v1.0")
 def test_running_list_table_names_action(
     context: CloudDataContext,
     cloud_base_url: str,
@@ -90,7 +89,6 @@ def test_running_list_table_names_action(
     )
 
 
-@pytest.mark.skip("Skipping integration tests until they are updated for v1.0")
 def test_running_list_table_names_action_fails_for_unreachable_datasource(
     context: CloudDataContext, cloud_base_url: str, org_id_env_var: str, token_env_var: str
 ):
@@ -110,5 +108,5 @@ def test_running_list_table_names_action_fails_for_unreachable_datasource(
 
     # Act & Assert
     # Check that the action was unsuccessful and an error was raised.
-    with pytest.raises(ValueError):
+    with pytest.raises(KeyError):
         action.run(event=list_table_names_event, id=event_id)
