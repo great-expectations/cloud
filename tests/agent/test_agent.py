64875cc2b2bee170fe00b7fb2cbc053a12c229be
William Shin <will@greatexpectations.io>
2024-10-11 16:29:00 -0700

    Update test_agent.py

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -279,13 +279,12 @@ def test_gx_agent_run_handles_subscriber_error_on_close(
     agent.run()
 
 
-@pytest.mark.unit
 def test_gx_agent_updates_cloud_on_job_status_error(
     subscriber, create_session, get_context, client, gx_agent_config, event_handler
 ):
     correlation_id = "4ae63677-4dd5-4fb0-b511-870e7a286e77"
     url = (
-        f"{gx_agent_config.gx_cloud_base_url}/organizations/"
+        f"{gx_agent_config.gx_cloud_base_url}/api/v1/organizations/"
         f"{gx_agent_config.gx_cloud_organization_id}/agent-jobs/{correlation_id}"
     )
     job_started_data = JobStarted().json()
@@ -417,7 +416,6 @@ def test_gx_agent_updates_cloud_on_job_status(
     )
 
 
-@pytest.mark.unit
 def test_gx_agent_handles_error_from_create_scheduled_job(
     subscriber,
     create_session,
@@ -433,7 +431,7 @@ def test_gx_agent_handles_error_from_create_scheduled_job(
     """
     correlation_id = "4ae63677-4dd5-4fb0-b511-870e7a286e77"
     post_url = (
-        f"{gx_agent_config.gx_cloud_base_url}/organizations/"
+        f"{gx_agent_config.gx_cloud_base_url}/api/v1/organizations/"
         f"{gx_agent_config.gx_cloud_organization_id}/agent-jobs"
     )
 
