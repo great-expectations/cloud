6c7646fd3cb1dd840198c5a8acb65651aacf2a77
Thu Pham <thu.pham@greatexpectations.io>
2024-10-25 15:22:37 -0700

    Update comment

--- great_expectations_cloud/agent/actions/utils.py
+++ great_expectations_cloud/agent/actions/utils.py
@@ -12,7 +12,8 @@ if TYPE_CHECKING:
 def get_table_names(datasource: SQLDatasource) -> list[str]:
     inspector: Inspector = inspect(datasource.get_engine())
     if isinstance(datasource, SnowflakeDatasource) and datasource.schema_:
-        # Snowflake-SQLAlchemy uses the default_schema if no schema is provided
+        # Snowflake-SQLAlchemy uses the default_schema if no schema is provided to get_table_names
+        # Or if the role does not have access to the schema (it silently fails and defaults to using default_schema)
         # See https://github.com/snowflakedb/snowflake-sqlalchemy/blob/e78319725d4b96ea205ef1264b744c65eb37853d/src/snowflake/sqlalchemy/snowdialect.py#L731
         # Explicitly passing the schema to the inspector to get the table names
         return inspector.get_table_names(schema=datasource.schema_)
