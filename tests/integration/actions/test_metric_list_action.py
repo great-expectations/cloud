e45b94f4900fb447f5c67e5bd034d5a955778577
William Shin <will@greatexpectations.io>
2024-11-19 14:16:01 -0800

    no stray pritn

--- tests/integration/actions/test_metric_list_action.py
+++ tests/integration/actions/test_metric_list_action.py
@@ -80,7 +80,6 @@ def graphql_test_client(
                 data=payload,
                 headers=headers,
             )
-            print(f"Response: {res}")
             if status_code is not None:
                 assert res.status_code == status_code
 
