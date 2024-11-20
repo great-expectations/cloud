5fdb2338bba6a32501dae3a180c090358ca61e07
Alena Hutchinson <alena.hutchinson@greatexpectations.io>
2024-11-20 15:20:13 -0800

    switch back to using env variable in context

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -31,10 +31,10 @@ def token_env_var() -> str:
 
 
 @pytest.fixture(scope="module")
-def context(cloud_base_url: str, org_id_env_var: str, token_env_var: str) -> CloudDataContext:
+def context(org_id_env_var: str, token_env_var: str) -> CloudDataContext:
     context = gx.get_context(
         mode="cloud",
-        cloud_base_url=cloud_base_url,
+        cloud_base_url=os.environ.get("GX_CLOUD_BASE_URL"),
         cloud_organization_id=org_id_env_var,
         cloud_access_token=token_env_var,
     )
