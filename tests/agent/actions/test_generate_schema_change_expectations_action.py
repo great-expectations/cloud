4e21cd5ed121d549ac2b6b39435fcb1670cbb71c
Anthony Burdi <anthony@greatexpectations.io>
2024-11-26 17:51:06 -0500

    Change wording in error message (#566)

--- tests/agent/actions/test_generate_schema_change_expectations_action.py
+++ tests/agent/actions/test_generate_schema_change_expectations_action.py
@@ -85,84 +85,6 @@ def mock_response_success(monkeypatch, mock_metrics_list: list[TableMetric]):
 
 
 @pytest.fixture
-def mock_response_failed_asset(monkeypatch, mock_metrics_list: list[TableMetric]):
-    def mock_data_asset(self, event: GenerateSchemaChangeExpectationsEvent, asset_name: str):
-        raise RuntimeError(f"Failed to retrieve asset: {asset_name}")  # noqa: TRY003 # following pattern in code
-
-    def mock_metrics(self, data_asset: DataAsset):
-        return MetricRun(metrics=mock_metrics_list)
-
-    def mock_schema_change_expectation(self, metric_run: MetricRun, expectation_suite_name: str):
-        return gx_expectations.ExpectTableColumnsToMatchSet(
-            column_set=["col1", "col2"], id=str(uuid.uuid4())
-        )
-
-    monkeypatch.setattr(
-        GenerateSchemaChangeExpectationsAction, "_retrieve_asset_from_asset_name", mock_data_asset
-    )
-    monkeypatch.setattr(GenerateSchemaChangeExpectationsAction, "_get_metrics", mock_metrics)
-    monkeypatch.setattr(
-        GenerateSchemaChangeExpectationsAction,
-        "_add_schema_change_expectation",
-        mock_schema_change_expectation,
-    )
-
-
-@pytest.fixture
-def mock_response_failed_metrics(monkeypatch):
-    def mock_data_asset(self, event: GenerateSchemaChangeExpectationsEvent, asset_name: str):
-        return TableAsset(
-            name="test-data-asset",
-            table_name="test_table",
-            schema_name="test_schema",
-        )
-
-    def mock_metrics(self, data_asset: DataAsset):
-        raise RuntimeError("One or more metrics failed to compute.")  # noqa: TRY003 # following pattern in code
-
-    def mock_schema_change_expectation(self, metric_run: MetricRun, expectation_suite_name: str):
-        return gx_expectations.ExpectTableColumnsToMatchSet(
-            column_set=["col1", "col2"], id=str(uuid.uuid4())
-        )
-
-    monkeypatch.setattr(
-        GenerateSchemaChangeExpectationsAction, "_retrieve_asset_from_asset_name", mock_data_asset
-    )
-    monkeypatch.setattr(GenerateSchemaChangeExpectationsAction, "_get_metrics", mock_metrics)
-    monkeypatch.setattr(
-        GenerateSchemaChangeExpectationsAction,
-        "_add_schema_change_expectation",
-        mock_schema_change_expectation,
-    )
-
-
-@pytest.fixture
-def mock_response_failed_schema_change(monkeypatch, mock_metrics_list: list[TableMetric]):
-    def mock_data_asset(self, event: GenerateSchemaChangeExpectationsEvent, asset_name: str):
-        return TableAsset(
-            name="test-data-asset",
-            table_name="test_table",
-            schema_name="test_schema",
-        )
-
-    def mock_metrics(self, data_asset: DataAsset):
-        return MetricRun(metrics=mock_metrics_list)
-
-    def mock_schema_change_expectation(self, metric_run: MetricRun, expectation_suite_name: str):
-        raise RuntimeError("Failed to add expectation to suite: test-suite")  # noqa: TRY003 # following pattern in code
-
-    monkeypatch.setattr(
-        GenerateSchemaChangeExpectationsAction, "_retrieve_asset_from_asset_name", mock_data_asset
-    )
-    monkeypatch.setattr(GenerateSchemaChangeExpectationsAction, "_get_metrics", mock_metrics)
-    monkeypatch.setattr(
-        GenerateSchemaChangeExpectationsAction,
-        "_add_schema_change_expectation",
-        mock_schema_change_expectation,
-    )
-
-
-@pytest.fixture
 def mock_multi_asset_success_and_failure(monkeypatch, mock_metrics_list: list[TableMetric]):
     def mock_data_asset(self, event: GenerateSchemaChangeExpectationsEvent, asset_name: str):
         if "retrieve-fail" in asset_name:
@@ -246,180 +168,18 @@ def test_generate_schema_change_expectations_action_success(
 
 
 @pytest.mark.parametrize(
-    "data_asset_names, expected_error_message",
-    [
-        (
-            ["test-data-asset1"],
-            "Failed to generate schema change expectations for 1 of the 1 assets.",
-        ),
-        (
-            ["test-data-asset1", "test-data-asset2"],
-            "Failed to generate schema change expectations for 2 of the 2 assets.",
-        ),
-    ],
-)
-def test_action_failure_in_retrieve_asset_from_asset_name(
-    mock_response_failed_asset,
-    mock_context: CloudDataContext,
-    mocker: MockerFixture,
-    data_asset_names: list[str],
-    expected_error_message: str,
-):
-    # setup
-    mock_metric_repository = mocker.Mock(spec=MetricRepository)
-    mock_batch_inspector = mocker.Mock(spec=BatchInspector)
-
-    action = GenerateSchemaChangeExpectationsAction(
-        context=mock_context,
-        metric_repository=mock_metric_repository,
-        batch_inspector=mock_batch_inspector,
-        base_url="",
-        auth_key="",
-        organization_id=uuid.uuid4(),
-    )
-
-    # run the action
-    with pytest.raises(PartialSchemaChangeExpectationError) as e:
-        action.run(
-            event=GenerateSchemaChangeExpectationsEvent(
-                type="generate_schema_change_expectations_request.received",
-                organization_id=uuid.uuid4(),
-                datasource_name="test-datasource",
-                data_assets=data_asset_names,
-                create_expectations=True,
-            ),
-            id="test-id",
-        )
-
-    # These are part of the same message
-    assert expected_error_message in str(e.value)
-    for asset_name in data_asset_names:
-        assert f"Asset: {asset_name}" in str(e.value)
-        assert f"Failed to retrieve asset: {asset_name}" in str(e.value)
-
-
-@pytest.mark.parametrize(
-    "data_asset_names, expected_error_message",
-    [
-        (
-            ["test-data-asset1"],
-            "Failed to generate schema change expectations for 1 of the 1 assets.",
-        ),
-        (
-            ["test-data-asset1", "test-data-asset2"],
-            "Failed to generate schema change expectations for 2 of the 2 assets.",
-        ),
-    ],
-)
-def test_action_failure_in_get_metrics(
-    mock_response_failed_metrics,
-    mock_context: CloudDataContext,
-    mocker: MockerFixture,
-    data_asset_names: list[str],
-    expected_error_message: str,
-):
-    # setup
-    mock_metric_repository = mocker.Mock(spec=MetricRepository)
-    mock_batch_inspector = mocker.Mock(spec=BatchInspector)
-
-    action = GenerateSchemaChangeExpectationsAction(
-        context=mock_context,
-        metric_repository=mock_metric_repository,
-        batch_inspector=mock_batch_inspector,
-        base_url="",
-        auth_key="",
-        organization_id=uuid.uuid4(),
-    )
-    # run the action
-    with pytest.raises(PartialSchemaChangeExpectationError) as e:
-        action.run(
-            event=GenerateSchemaChangeExpectationsEvent(
-                type="generate_schema_change_expectations_request.received",
-                organization_id=uuid.uuid4(),
-                datasource_name="test-datasource",
-                data_assets=data_asset_names,
-                create_expectations=True,
-            ),
-            id="test-id",
-        )
-
-    # These are part of the same message
-    assert expected_error_message in str(e.value)
-    for asset_name in data_asset_names:
-        assert f"Asset: {asset_name}" in str(e.value)
-        assert "One or more metrics failed to compute." in str(e.value)
-
-
-@pytest.mark.parametrize(
-    "data_asset_names, expected_error_message",
-    [
-        (
-            ["test-data-asset1"],
-            "Failed to generate schema change expectations for 1 of the 1 assets.",
-        ),
-        (
-            ["test-data-asset1", "test-data-asset2"],
-            "Failed to generate schema change expectations for 2 of the 2 assets.",
-        ),
-    ],
-)
-def test_action_failure_in_add_schema_change_expectation(
-    mock_response_failed_schema_change,
-    mock_context: CloudDataContext,
-    mocker: MockerFixture,
-    data_asset_names: list[str],
-    expected_error_message: str,
-):
-    # setup
-    mock_metric_repository = mocker.Mock(spec=MetricRepository)
-    mock_batch_inspector = mocker.Mock(spec=BatchInspector)
-
-    action = GenerateSchemaChangeExpectationsAction(
-        context=mock_context,
-        metric_repository=mock_metric_repository,
-        batch_inspector=mock_batch_inspector,
-        base_url="",
-        auth_key="",
-        organization_id=uuid.uuid4(),
-    )
-
-    # run the action
-    with pytest.raises(PartialSchemaChangeExpectationError) as e:
-        action.run(
-            event=GenerateSchemaChangeExpectationsEvent(
-                type="generate_schema_change_expectations_request.received",
-                organization_id=uuid.uuid4(),
-                datasource_name="test-datasource",
-                data_assets=data_asset_names,
-                create_expectations=True,
-            ),
-            id="test-id",
-        )
-
-    # These are part of the same message
-    assert expected_error_message in str(e.value)
-    for asset_name in data_asset_names:
-        assert f"Asset: {asset_name}" in str(e.value)
-        assert "Failed to add expectation to suite: test-suite" in str(e.value)
-
-
-@pytest.mark.parametrize(
-    "succeeding_data_asset_names, failing_data_asset_names, failing_data_asset_error_messages, expected_error_message, expected_truncation_message",
+    "succeeding_data_asset_names, failing_data_asset_names, error_message_header",
     [
         pytest.param(
             ["test-data-asset1"],
             ["retrieve-fail-asset-1"],
-            ["Failed to retrieve asset: retrieve-fail-asset-1"],
-            "Failed to generate schema change expectations for 1 of the 2 assets.",
-            "",
+            "Unable to fetch schemas for 1 of 2 Data Assets.",
             id="Single asset passing, single asset failing",
         ),
         pytest.param(
             ["test-data-asset1", "test-data-asset2"],
             ["retrieve-fail-asset-1"],
-            ["Failed to retrieve asset: retrieve-fail-asset-1"],
-            "Failed to generate schema change expectations for 1 of the 3 assets.",
-            "",
+            "Unable to fetch schemas for 1 of 3 Data Assets.",
             id="Multiple assets passing, single asset failing",
         ),
         pytest.param(
@@ -430,39 +190,9 @@ def test_action_failure_in_add_schema_change_expectation(
                 "metric-fail-asset-1",
                 "metric-fail-asset-2",
             ],
-            [
-                "Failed to retrieve asset: retrieve-fail-asset-1",
-                "Failed to retrieve asset: retrieve-fail-asset-2",
-                "One or more metrics failed to compute.",
-                "One or more metrics failed to compute.",
-            ],
-            "Failed to generate schema change expectations for 4 of the 6 assets.",
-            "",
+            "Unable to fetch schemas for 4 of 6 Data Assets.",
             id="Multiple assets passing, multiple assets failing",
         ),
-        pytest.param(
-            ["test-data-asset1", "test-data-asset2"],
-            [
-                "retrieve-fail-asset-1",
-                "retrieve-fail-asset-2",
-                "metric-fail-asset-1",
-                "metric-fail-asset-2",
-                "schema-fail-asset-1",
-                "schema-fail-asset-2",
-            ],
-            [
-                "Failed to retrieve asset: retrieve-fail-asset-1",
-                "Failed to retrieve asset: retrieve-fail-asset-2",
-                "One or more metrics failed to compute.",
-                "One or more metrics failed to compute.",
-                "Failed to add expectation to suite: test-suite",
-                "Failed to add expectation to suite: test-suite",
-            ],
-            "Failed to generate schema change expectations for 6 of the 8 assets.",
-            "Only displaying the first 5 errors. There is 1 additional error.",
-            id="Multiple assets passing, multiple assets failing, one more than max display errors",
-        ),
-        # More than one more than max errors to display
         pytest.param(
             ["test-data-asset1", "test-data-asset2"],
             [
@@ -476,19 +206,7 @@ def test_action_failure_in_add_schema_change_expectation(
                 "schema-fail-asset-4",
                 "schema-fail-asset-5",
             ],
-            [
-                "Failed to retrieve asset: retrieve-fail-asset-1",
-                "Failed to retrieve asset: retrieve-fail-asset-2",
-                "One or more metrics failed to compute.",
-                "One or more metrics failed to compute.",
-                "Failed to add expectation to suite: test-suite",
-                "Failed to add expectation to suite: test-suite",
-                "Failed to add expectation to suite: test-suite",
-                "Failed to add expectation to suite: test-suite",
-                "Failed to add expectation to suite: test-suite",
-            ],
-            "Failed to generate schema change expectations for 9 of the 11 assets.",
-            "Only displaying the first 5 errors. There are 4 additional errors.",
+            "Unable to fetch schemas for 9 of 11 Data Assets.",
             id="Multiple assets passing, multiple assets failing, more than max display errors",
         ),
     ],
@@ -499,9 +217,7 @@ def test_succeeding_and_failing_assets_together(
     mocker: MockerFixture,
     succeeding_data_asset_names: list[str],
     failing_data_asset_names: list[str],
-    failing_data_asset_error_messages: list[str],
-    expected_error_message: str,
-    expected_truncation_message: str,
+    error_message_header: str,
 ):
     # setup
     mock_metric_repository = mocker.Mock(spec=MetricRepository)
@@ -530,13 +246,8 @@ def test_succeeding_and_failing_assets_together(
         )
 
     # These are part of the same message
-    assert expected_error_message in str(e.value)
-    if expected_truncation_message:
-        assert expected_truncation_message in str(e.value)
-    else:
-        # If there is no truncation message, we don't know which errors are displayed and in which order.
-        # So we just check that the error messages are in the exception message
-        # if there is no truncation message.
-        for idx, asset_name in enumerate(failing_data_asset_names):
-            assert f"Asset: {asset_name}" in str(e.value)
-            assert failing_data_asset_error_messages[idx] in str(e.value)
+    assert error_message_header in str(e.value)
+    error_message_footer = "Check your connection details, delete and recreate these Data Assets."
+    assert error_message_footer in str(e.value)
+    for asset_name in failing_data_asset_names:
+        assert asset_name in str(e.value)
