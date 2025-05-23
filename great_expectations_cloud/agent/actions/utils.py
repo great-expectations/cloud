c80aa7f8e4ac50b30d494b45e4f89cf8f75e557c
Łukasz Lempart <lukasz@greatexpectations.io>
2025-05-23 08:50:55 -0700

    Fetch views when listing assets in a database

--- great_expectations_cloud/agent/actions/utils.py
+++ great_expectations_cloud/agent/actions/utils.py
@@ -15,8 +15,12 @@ def get_table_names(datasource: SQLDatasource) -> list[str]:
         # Snowflake-SQLAlchemy uses the default_schema if no schema is provided to get_table_names
         # Or if the role does not have access to the schema (it silently fails and defaults to using default_schema)
         # See https://github.com/snowflakedb/snowflake-sqlalchemy/blob/e78319725d4b96ea205ef1264b744c65eb37853d/src/snowflake/sqlalchemy/snowdialect.py#L731
-        # Explicitly passing the schema to the inspector to get the table names
+        # Explicitly passing the schema to the inspector to get the table and view names
         # Also converting to list to ensure JSON serializable
-        return list(inspector.get_table_names(schema=datasource.schema_))
+        tables = list(inspector.get_table_names(schema=datasource.schema_))
+        views = list(inspector.get_view_names(schema=datasource.schema_))
+        return tables + views
 
-    return list(inspector.get_table_names())
+    tables = list(inspector.get_table_names())
+    views = list(inspector.get_view_names())
+    return tables + views
--- tests/agent/actions/test_utils.py
+++ tests/agent/actions/test_utils.py
@@ -13,12 +13,14 @@ def test_get_table_names_with_sql_datasource(mocker):
     inspector = mocker.Mock()
     mock_sqlalchemy_inspect.return_value = inspector
     inspector.get_table_names.return_value = ["table_1", "table_2"]
+    inspector.get_view_names.return_value = ["view_1", "view_2"]
 
     table_names = get_table_names(datasource)
 
-    assert table_names == ["table_1", "table_2"]
+    assert table_names == ["table_1", "table_2", "view_1", "view_2"]
     mock_sqlalchemy_inspect.assert_called_once_with(engine)
     inspector.get_table_names.assert_called_once_with()
+    inspector.get_view_names.assert_called_once_with()
 
 
 def test_get_table_names_with_snowflake_datasource(mocker):
@@ -30,12 +32,14 @@ def test_get_table_names_with_snowflake_datasource(mocker):
     inspector = mocker.Mock()
     mock_sqlalchemy_inspect.return_value = inspector
     inspector.get_table_names.return_value = ["table_1", "table_2"]
+    inspector.get_view_names.return_value = ["view_1", "view_2"]
 
     table_names = get_table_names(datasource)
 
-    assert table_names == ["table_1", "table_2"]
+    assert table_names == ["table_1", "table_2", "view_1", "view_2"]
     mock_sqlalchemy_inspect.assert_called_once_with(engine)
     inspector.get_table_names.assert_called_once_with(schema="test_schema")
+    inspector.get_view_names.assert_called_once_with(schema="test_schema")
 
 
 def test_get_table_names_with_snowflake_datasource_no_schema(mocker):
@@ -47,9 +51,11 @@ def test_get_table_names_with_snowflake_datasource_no_schema(mocker):
     inspector = mocker.Mock()
     mock_sqlalchemy_inspect.return_value = inspector
     inspector.get_table_names.return_value = ["table_1", "table_2"]
+    inspector.get_view_names.return_value = ["view_1", "view_2"]
 
     table_names = get_table_names(datasource)
 
-    assert table_names == ["table_1", "table_2"]
+    assert table_names == ["table_1", "table_2", "view_1", "view_2"]
     mock_sqlalchemy_inspect.assert_called_once_with(engine)
     inspector.get_table_names.assert_called_once_with()
+    inspector.get_view_names.assert_called_once_with()
