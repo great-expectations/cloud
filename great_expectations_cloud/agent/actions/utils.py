86516cf69318327eb0c01e270b06845bdb8a7c7f
Steffen Lehmann <Steffen.Lehmann@bci.ca>
2025-06-02 15:28:36 -0700

    Merge upstream

--- great_expectations_cloud/agent/actions/utils.py
+++ great_expectations_cloud/agent/actions/utils.py
@@ -12,29 +12,27 @@ if TYPE_CHECKING:
     from sqlalchemy.engine import Inspector
 
 
-<<<<<<< HEAD
-def get_table_names(datasource: SQLDatasource) -> list[str]:
-    engine = datasource.get_engine()
-    inspector: Inspector = inspect(engine)
-=======
 def get_asset_names(datasource: SQLDatasource) -> list[str]:
     inspector: Inspector = inspect(datasource.get_engine())
->>>>>>> upstream/main
     if isinstance(datasource, SnowflakeDatasource) and datasource.schema_:
         # Snowflake-SQLAlchemy uses the default_schema if no schema is provided to get_table_names
         # Or if the role does not have access to the schema (it silently fails and defaults to using default_schema)
         # See https://github.com/snowflakedb/snowflake-sqlalchemy/blob/e78319725d4b96ea205ef1264b744c65eb37853d/src/snowflake/sqlalchemy/snowdialect.py#L731
         # Explicitly passing the schema to the inspector to get the table and view names
         # Also converting to list to ensure JSON serializable
-<<<<<<< HEAD
-        return list(inspector.get_table_names(schema=datasource.schema_))
+        tables = list(inspector.get_table_names(schema=datasource.schema_))
+        views = list(inspector.get_view_names(schema=datasource.schema_))
+        return tables + views
     
+    engine = datasource.get_engine()
     if engine.dialect.name == GXSqlDialect.MSSQL:
         tables = list(chain.from_iterable(list(map(lambda o: f"{schema}.{o}", inspector.get_table_names(schema=schema))) for schema in inspector.get_schema_names()))
         views = list(chain.from_iterable(list(map(lambda o: f"{schema}.{o}", inspector.get_view_names(schema=schema))) for schema in inspector.get_schema_names()))
         return tables + views
 
-    return list(inspector.get_table_names())
+    tables = list(inspector.get_table_names())
+    views = list(inspector.get_view_names())
+    return tables + views
 
 def verify_data_asset(log, datasource: SQLDatasource, asset):
     try:
@@ -44,12 +42,3 @@ def verify_data_asset(log, datasource: SQLDatasource, asset):
             datasource.add_table_asset(name = asset.name, schema_name = asset.name.split('.')[0], table_name = asset.name.split('.')[1])
     except Exception as e:
         log(f"Failed to verify asset {asset.name}: {e}")
-=======
-        tables = list(inspector.get_table_names(schema=datasource.schema_))
-        views = list(inspector.get_view_names(schema=datasource.schema_))
-        return tables + views
-
-    tables = list(inspector.get_table_names())
-    views = list(inspector.get_view_names())
-    return tables + views
->>>>>>> upstream/main
